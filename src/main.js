2a3a57b709723141d877358e738d444900e461ee
Zyuhel <zyuhel@yandex.ru>
2017-09-23 12:45:05 +0300

    adding vue-material library for ui and renaming to haze

--- src/main.js
+++ src/main.js
@@ -3,8 +3,31 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'vue-material/dist/vue-material.css'
+
+var VueMaterial = require('vue-material')
+
+Vue.use(VueMaterial)
 
 Vue.config.productionTip = false
+
+Vue.material.registerTheme({
+  default: {
+    primary: {
+      color: 'light-green',
+      hue: 700
+    },
+    accent: 'red'
+  },
+  teal: {
+    primary: 'blue',
+    accent: 'pink'
+  },
+  purple: {
+    primary: 'purple',
+    accent: 'orange'
+  }
+})
 
 /* eslint-disable no-new */
 new Vue({
