988f8e7f7c29a5588a62a2aa163e4a61f77707aa
Zyuhel <zyuhel@yandex.ru>
2017-11-28 03:45:19 +0300

    resort chats messages by date, latest first. May be it should be put in options

--- src/components/Chats.vue
+++ src/components/Chats.vue
@@ -19,14 +19,15 @@
             <md-list-item v-if="firstLoading">
                 <md-spinner :md-size="150" :md-stroke="1" md-indeterminate class="md-accent"></md-spinner>
             </md-list-item>
-            <md-list-item v-for="(chat, address) in chatList" :key="address" v-on:click="$router.push('/chats/' + address + '/')">
+            <md-list-item v-for="(chat, address) in chatList" :key="chat.partner" v-on:click="$router.push('/chats/' + chat.partner + '/')">
                 <md-avatar class="md-avatar-icon">
                     <md-icon>library_books</md-icon>
                 </md-avatar>
 
                 <div class="md-list-text-container">
-                    <span>{{ chatName(address) }}</span>
+                    <span>{{ chatName(chat.partner) }}</span>
                     <p v-html="chat.last_message.message"></p>
+                    <span class="dt">{{ dateFormat(chat.last_message.timestamp) }}</span>
                 </div>
 
 
@@ -51,6 +52,11 @@ export default {
         return this.$store.state.partners[address]
       }
       return address
+    },
+    dateFormat: function (timestamp) {
+      var date = new Date(parseInt(timestamp) * 1000 + Date.UTC(2017, 8, 2, 17, 0, 0, 0))
+      var options = {'weekday': 'short'}
+      return date.toLocaleDateString('ru-RU', options) + ', ' + date.toLocaleTimeString().substring(0, 5)
     }
   },
   computed: {
@@ -58,6 +64,18 @@ export default {
       return this.$store.state.firstChatLoad
     },
     chatList: function () {
+      function compare (a, b) {
+        if (a.last_message.timestamp < b.last_message.timestamp) {
+          return 1
+        }
+        if (a.last_message.timestamp > b.last_message.timestamp) {
+          return -1
+        }
+        return 0
+      }
+      if (this.$store.state.chats) {
+        return Object.values(this.$store.state.chats).sort(compare)
+      }
       return this.$store.state.chats
     }
   },
@@ -74,6 +92,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
+    .md-list-text-container .dt {
+        position: absolute;
+        top: 0;
+        right: 5px;
+        font-size: 8px;
+        font-style: italic;
+    }
     .md-avatar .new-icon {
         position: absolute;
         right: -5px;
