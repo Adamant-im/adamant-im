2426fc751ff8449cac66c96e309bebed5b96591e
Zyuhel <zyuhel@yandex.ru>
2017-12-20 14:04:16 +0300

    accept constant messages only from allowed wallets, to protect from scam.

--- src/lib/adamantServerApi.js
+++ src/lib/adamantServerApi.js
@@ -386,13 +386,19 @@ function install (Vue) {
           var message = new Uint8Array(this.hexToBytes(currentTransaction.asset.chat.message))
           var nonce = new Uint8Array(this.hexToBytes(currentTransaction.asset.chat.own_message))
           currentTransaction.message = this.decodeMessage(message, decodePublic, nonce)
-          if (currentTransaction.message === 'chats.welcome_message' || currentTransaction.message === 'chats.preico_message') {
+          if ((currentTransaction.message.indexOf('chats.welcome_message') > -1 && currentTransaction.senderId === 'U15423595369615486571') || (currentTransaction.message.indexOf('chats.preico_message') > -1 && currentTransaction.senderId === 'U7047165086065693428')) {
 //            currentTransaction.message = this.$i18n.t('chats.welcome_message')
           } else {
             currentTransaction.message = marked(currentTransaction.message, {renderer: renderer})
           }
           if (currentTransaction.message && currentTransaction.message.length > 0) {
-            if (currentTransaction.message === 'chats.welcome_message' || currentTransaction.message === 'chats.preico_message') {
+            if ((currentTransaction.message.indexOf('chats.welcome_message') > -1 && currentTransaction.senderId === 'U15423595369615486571') || (currentTransaction.message.indexOf('chats.preico_message') > -1 && currentTransaction.senderId === 'U7047165086065693428')) {
+              if (currentTransaction.senderId === 'U15423595369615486571') {
+                currentTransaction.message = 'chats.welcome_message'
+              }
+              if (currentTransaction.senderId === 'U7047165086065693428') {
+                currentTransaction.message = 'chats.preico_message'
+              }
               this.$store.dispatch('add_chat_i18n_message', currentTransaction)
             } else {
               this.$store.commit('add_chat_message', currentTransaction)
@@ -406,13 +412,19 @@ function install (Vue) {
         var message = new Uint8Array(this.hexToBytes(currentTransaction.asset.chat.message))
         var nonce = new Uint8Array(this.hexToBytes(currentTransaction.asset.chat.own_message))
         currentTransaction.message = this.decodeMessage(message, decodePublic, nonce)
-        if (currentTransaction.message === 'chats.welcome_message' || currentTransaction.message === 'chats.preico_message') {
+        if ((currentTransaction.message.indexOf('chats.welcome_message') > -1 && currentTransaction.senderId === 'U15423595369615486571') || (currentTransaction.message.indexOf('chats.preico_message') > -1 && currentTransaction.senderId === 'U7047165086065693428')) {
 //          currentTransaction.message = this.$i18n.t('chats.welcome_message')
         } else {
           currentTransaction.message = marked(currentTransaction.message, {renderer: renderer})
         }
         if (currentTransaction.message && currentTransaction.message.length > 0) {
-          if (currentTransaction.message === 'chats.welcome_message' || currentTransaction.message === 'chats.preico_message') {
+          if ((currentTransaction.message.indexOf('chats.welcome_message') > -1 && currentTransaction.senderId === 'U15423595369615486571') || (currentTransaction.message.indexOf('chats.preico_message') > -1 && currentTransaction.senderId === 'U7047165086065693428')) {
+            if (currentTransaction.senderId === 'U15423595369615486571') {
+              currentTransaction.message = 'chats.welcome_message'
+            }
+            if (currentTransaction.senderId === 'U7047165086065693428') {
+              currentTransaction.message = 'chats.preico_message'
+            }
             this.$store.dispatch('add_chat_i18n_message', currentTransaction)
           } else {
             this.$store.commit('add_chat_message', currentTransaction)
