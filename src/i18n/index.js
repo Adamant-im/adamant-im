c47ce8b2bf3be32b390db7296c2f488493d9ffce
Zyuhel <zyuhel@yandex.ru>
2017-12-24 16:18:51 +0300

    adding Filipino translation

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,9 +1,11 @@
 import en from './en'
 import ru from './ru'
+import ph from './ph'
 import zhtw from './zh-tw'
 
 export default {
   'en': en,
   'zh-tw': zhtw,
-  'ru': ru
+  'ru': ru,
+  'ph': ph
 }
