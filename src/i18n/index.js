6a17ee82d78d0da0ed0275bbdd60e0402f47c702
Zyuhel <zyuhel@yandex.ru>
2017-12-28 13:05:14 +0300

    adding Hindi translation

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,5 +1,6 @@
 import en from './en'
 import ru from './ru'
+import hi from './hi'
 import ph from './ph'
 import zhtw from './zh-tw'
 
@@ -7,5 +8,6 @@ export default {
   'en': en,
   'zh-tw': zhtw,
   'ru': ru,
-  'ph': ph
+  'ph': ph,
+  'hi': hi
 }
