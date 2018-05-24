3c7b756b9b60bc299a1df877914c9302ba43b7d8
Alexander Kiselev <alex.v.kiselev@gmail.com>
2018-05-25 02:06:57 +0300

    Ensure Ethereum API client is unlocked to survive the page reload

--- src/store/modules/eth/actions.js
+++ src/store/modules/eth/actions.js
@@ -5,13 +5,20 @@ const servers = config.server.eth
 const server = servers[Math.floor(Math.random() * servers.length)]
 const api = new EthApi(server)
 
+function unlockApi (context) {
+  if (api.isUnlocked) return
+  const passphrase = context.rootState.passPhrase
+  api.unlock(passphrase)
+}
+
 export default {
   login (context, passphrase) {
     api.unlock(passphrase)
-    context.commit('address', api.getAddress())
+    context.commit('address', api.address)
   },
 
   updateBalance (context) {
+    unlockApi(context)
     api.getBalance().then(
       balance => context.commit('balance', balance),
       error => console.error('Failed to update ETH balance', error)
