960dc5f3f7aea7c3beb512abf3bdde71a12bcbeb
Sergey Ushakov <sergushakov.public@gmail.com>
2018-06-14 23:25:28 +0300

    votes table behavior fixed

--- src/components/Votes.vue
+++ src/components/Votes.vue
@@ -28,26 +28,38 @@
               </md-table-row>
             </md-table-header>
             <md-table-body v-bind:style="tableStyle">
-              <md-table-row v-for="(delegate, index) in delegates" :key="delegate.address" style="cursor:pointer" v-bind:class="{upvoted: delegate.upvoted, downvoted: delegate.downvoted}">
-                <md-table-cell>
-                  <md-checkbox v-model="delegate.voted" title="vote" v-on:change="vote(delegate)"></md-checkbox>
-                </md-table-cell>
-                <md-table-cell style="text-align: left">{{ delegate.rank }}</md-table-cell>
-                <md-table-cell style="text-align: left"><span>{{ delegate.username }}</span></md-table-cell>
-                <md-table-cell>
-                  <span v-on:click="toggleDetails(delegate)"><md-icon>{{ delegate.showDetails ? 'chevron_left' : 'chevron_right' }}</md-icon></span>
-                  <md-list v-show="delegate.showDetails">
-                    <md-list-item class="md-inset">
+              <template v-for="(delegate, index) in delegates">
+                <md-table-row style="cursor:pointer" :key="delegate.address" v-bind:class="{upvoted: delegate.upvoted, downvoted: delegate.downvoted}" @click.native="toggleDetails(delegate)">
+                  <md-table-cell>
+                    <md-checkbox v-model="delegate.voted" title="vote" v-on:change="vote(delegate)"></md-checkbox>
+                  </md-table-cell>
+                  <md-table-cell style="text-align: left">{{ delegate.rank }}</md-table-cell>
+                  <md-table-cell style="text-align: left"><span>{{ delegate.username }}</span></md-table-cell>
+                  <md-table-cell>
+                    <md-icon>{{ delegate.showDetails ? 'show_less' : 'chevron_right' }}</md-icon>
+                  </md-table-cell>
+                </md-table-row>
+                <md-table-row v-show="delegate.showDetails">
+                  <md-table-cell colspan="4" class="delegate-details">
+                    <span class="block">
                       <span class="status-indicator" v-bind:style="delegate.style" v-bind:title="delegate.tooltip"></span>
                       <a target="_blank" v-bind:href="'https://explorer.adamant.im/delegate/'+delegate.address">{{ delegate.address }}</a>
-                    </md-list-item>
-                    <md-list-item class="md-inset">{{$t('votes.delegate_approval')}}: <strong>{{ delegate.approval }}%</strong></md-list-item>
-                    <md-list-item class="md-inset">{{$t('votes.delegate_uptime')}}: <strong>{{ delegate.productivity }}%</strong></md-list-item>
-                    <md-list-item class="md-inset">{{$t('votes.delegate_forged')}}: <strong>{{ (delegate.forged  / 100000000).toFixed(4) }} ADM</strong></md-list-item>
-                    <md-list-item class="md-inset">{{$t('votes.delegate_forging_time')}}: <strong>{{ formatForgingTime(delegate.forgingTime) }}</strong></md-list-item>
-                  </md-list>
-                </md-table-cell>
-              </md-table-row>
+                    </span>
+                    <span class="block">
+                      {{$t('votes.delegate_approval')}}: <strong>{{ delegate.approval }}%</strong>
+                    </span>
+                    <span class="block">
+                      {{$t('votes.delegate_uptime')}}: <strong>{{ delegate.productivity }}%</strong>
+                    </span>
+                    <span class="block">
+                      {{$t('votes.delegate_forged')}}: <strong>{{ (delegate.forged  / 100000000).toFixed(4) }} ADM</strong>
+                    </span>
+                    <span class="block">
+                      {{$t('votes.delegate_forging_time')}}: <strong>{{ formatForgingTime(delegate.forgingTime) }}</strong>
+                    </span>
+                  </md-table-cell>
+                </md-table-row>
+              </template>
             </md-table-body>
           </md-table>
         </div>
@@ -135,9 +147,6 @@
         }
         delegate.showDetails = !delegate.showDetails
       },
-      testClick (delegate) {
-        alert(delegate.address)
-      },
       formatForgingTime (seconds) {
         if (!seconds) {
           return '...'
@@ -163,6 +172,9 @@
       },
       formatHeight (height) {
         return `${height * 0.6}px !important`
+      },
+      testC (delegate) {
+        alert('Wooopiiiiie!, ' + delegate.username)
       }
     },
     computed: {
@@ -199,7 +211,7 @@
           return 0
         }
         const filterDelegates = (x) => {
-          const regexp = new RegExp(this.filterString)
+          const regexp = new RegExp(this.filterString, 'i')
           return this.filterString !== '' ? (regexp.test(x.address) || regexp.test(x.username)) : true
         }
 
@@ -251,7 +263,7 @@
   display: block;
 }
 .votes .md-table .md-table-row:hover .md-table-cell {
-  background-color: none;
+  background-color: transparent !important;
 }
 .votes .md-table tr:hover {
   background-color: #eee;
@@ -271,11 +283,15 @@
 .votes .md-checkbox {
   margin: auto;
 }
-
 .votes .md-toolbar.md-theme-grey {
   border-bottom: none;
 }
-
+.votes .delegate-details {
+  width: 100% !important;
+}
+.votes .delegate-details .block {
+  padding: 0 10px;
+}
 .md-table-row.downvoted {
   background-color: antiquewhite;
 }
