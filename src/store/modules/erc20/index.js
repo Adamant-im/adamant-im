59cc6ba571962034f9b6cf8dab8da3461d63954f
Alexander Kiselev <AKiselev4@alfabank.ru>
2018-08-21 12:01:40 +0300

    Dummy commit to restart Travis build

--- src/store/modules/erc20/index.js
+++ src/store/modules/erc20/index.js
@@ -3,6 +3,12 @@ import initialState from './erc20-state'
 import getters from './erc20-getters'
 import mutations from './erc20-mutations'
 
+/**
+ * Creates new ERC20 module.
+ * @param {String} crypto crypto name
+ * @param {*} contractAddress ERC20 contract address
+ * @param {*} decimals number of decimals for the amounts
+ */
 export default function erc20Module (crypto, contractAddress, decimals) {
   return {
     namespaced: true,
