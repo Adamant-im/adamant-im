0c8bd2977779c6539a166a61aab9a60de180d93d
Mikhail Efimov <realgoodprogrammer@gmail.com>
2018-09-15 22:47:30 +0300

    Bugfix: rotate cam icon was invisible

--- src/components/QRScan.vue
+++ src/components/QRScan.vue
@@ -12,7 +12,7 @@
           <md-button style="max-width: 40px; min-width: 40px; margin-right: 0; padding: 0" v-for="(camera) in cameras" :key="camera.id"
                      v-if="cameras.length > 1 && camera.id !== activeCameraId"
                      :title="camera.name" @click.stop="selectCamera(camera)">
-            <md-icon md-src="/static/img/Attach/rotate-cam.svg"></md-icon>
+            <md-icon md-src="/img/Attach/rotate-cam.svg"></md-icon>
           </md-button>
           <md-button class="md-raised" @click="hideModal"> {{ $t('scan.close_button') }} </md-button>
         </div>
