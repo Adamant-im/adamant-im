2b5e9c4ea0d9c760dd951bf94ea0333d2b5dae93
bluder <victorcpla@gmail.com>
2018-11-18 21:18:45 +0200

    feat(icon): better SVG icon component

---
+++ src/components/icons/BaseIcon.vue
@@ -0,0 +1,48 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg" 
+    :width="width" 
+    :height="height" 
+    :viewBox="viewBox" 
+    :aria-labelledby="title" 
+    role="presentation"
+  >
+    <title v-if="title">{{ title }}</title>
+    <g :fill="color">
+      <slot/>
+    </g>
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    width: {
+      type: [Number, String],
+      default: 32
+    },
+    height: {
+      type: [Number, String],
+      default: 32
+    },
+    title: {
+      type: String
+    },
+    color: {
+      type: String,
+      default: 'currentColor'
+    },
+    viewBox: {
+      type: String,
+      default: '0 0 512 512'
+    }
+  }
+}
+</script>
+
+<style scoped>
+svg {
+  display: inline-block;
+  vertical-align: baseline;
+}
+</style>
