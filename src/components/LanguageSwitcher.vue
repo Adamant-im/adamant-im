b88a32ef12ba88a0ee5004bbd83280fd57e04b43
bluder <victorcpla@gmail.com>
2018-11-23 19:32:54 +0200

    fix(LanguageSwitcher): link to store

--- src/components/LanguageSwitcher.vue
+++ src/components/LanguageSwitcher.vue
@@ -5,48 +5,68 @@
       slot="activator"
       flat
     >
-      {{ currentLanguage }}
+      {{ currentLanguageName }}
       <v-icon right>mdi-chevron-down</v-icon>
     </v-btn>
     <v-list>
       <v-list-tile
-        v-for="(language, code) in languages"
-        :key="code"
-        @click="onSelect(code)"
+        v-for="language in languages"
+        :key="language.locale"
+        @click="onSelect(language.locale)"
       >
-        <v-list-tile-title>{{ language.title }}</v-list-tile-title>
+        <v-list-tile-title>{{ language.name }}</v-list-tile-title>
       </v-list-tile>
     </v-list>
   </v-menu>
 </template>
 
 <script>
-import i18n from '../i18n'
-
 export default {
-  mounted () {
-    this.currentLanguage = this.$i18n.locale
-  },
   computed: {
-    languages () {
-      return i18n.messages
-    },
-    currentLanguage: {
+    currentLocale: {
       get () {
-        const languageCode = Object.keys(this.languages)
-          .find(code => code === this.$i18n.locale)
-
-        return this.languages[languageCode].title
+        return this.$store.state.language.currentLocale
       },
-      set (languageCode) {
-        this.$i18n.locale = languageCode
-        this.$store.commit('change_lang', languageCode)
+      set (value) {
+        this.$store.dispatch('language/changeLocale', value)
       }
+    },
+    currentLanguageName () {
+      const language = this.languages.find(language => language.locale === this.currentLocale)
+
+      return language.name
+    }
+  },
+  data () {
+    return {
+      // @todo may need to be transferred to i18n
+      languages: [
+        {
+          name: 'العربية',
+          locale: 'ar'
+        },
+        {
+          name: 'Deutsch',
+          locale: 'de'
+        },
+        {
+          name: 'English',
+          locale: 'en'
+        },
+        {
+          name: 'Française',
+          locale: 'fr'
+        },
+        {
+          name: 'Русский',
+          locale: 'ru'
+        }
+      ]
     }
   },
   methods: {
-    onSelect (languageCode) {
-      this.currentLanguage = languageCode
+    onSelect (locale) {
+      this.currentLocale = locale
     }
   }
 }
