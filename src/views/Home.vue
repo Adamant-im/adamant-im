04315afd24c54d647f58c8e337bb6a892a2fe112
bluder <victorcpla@gmail.com>
2018-11-27 10:56:15 +0200

    feat(): add Wallet View

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,189 +1,140 @@
 <template>
-  <!--TODO: absolute paths must be relative again-->
-  <div class="home" v-if="isLogged">
-      <md-layout md-align="center" md-gutter="16">
-      <md-list class="md-double-line md-transparent">
-          <md-list-item
-            v-for="addr in wallets"
-            :key="'addr_' + addr.system"
-            @click="copy"
-            v-clipboard="addr.address"
-            @success="copySuccess"
-            :title="addr.tooltip ? $t(addr.tooltip) : ''"
-          >
-              <md-avatar class="md-avatar-icon">
-                <md-icon :md-src="addr.addressIcon"></md-icon>
-              </md-avatar>
+  <v-layout row wrap justify-center>
+    <v-flex xs12 md4>
+      <v-card flat class="transparent white--text">
 
-              <div class="md-list-text-container for-address">
-                  <div class="block_entry_title">{{ $t('home.your_address_'+addr.system) }}</div>
-                  <p> {{ addr.address }}</p>
-              </div>
+        <!-- Wallets -->
+        <v-list two-line class="transparent">
+          <v-subheader>
+            Wallets
+          </v-subheader>
 
-              <div class='md-list-action'>
-                <md-icon class="custom-icon">content_copy</md-icon>
-              </div>
-          </md-list-item>
-
-          <md-list-item
+          <v-list-tile
             v-for="wallet in wallets"
-            v-bind:key="'bal_' + wallet.system"
-            v-on:click="goToTransactions(wallet.system)"
-            :title="wallet.balanceTooltip ? $t(wallet.balanceTooltip) : ''"
+            :key="wallet.system"
+            avatar
+            @click="copyToClipboard(wallet.walletAddress)"
           >
-            <md-avatar class="md-avatar-icon">
-                <md-icon :md-src="wallet.balanceIcon"></md-icon>
-            </md-avatar>
-            <div class="md-list-text-container">
-                <span>{{ $t('home.your_balance_' + wallet.system) }}</span>
-                <p> <span v-html="wallet.balance"></span> {{ wallet.system }}</p>
-            </div>
-          </md-list-item>
+            <v-list-tile-avatar>
+              <v-icon class="grey lighten-1 white--text">{{ wallet.iconWallet }}</v-icon>
+            </v-list-tile-avatar>
 
-          <md-list-item v-on:click="$router.push('/transfer/')" :title="$t('home.send_btn_tooltip')">
-              <md-avatar class="md-avatar-icon">
-                <md-icon md-src="/img/Wallet/send.svg"></md-icon>
-              </md-avatar>
-              <div class="md-list-text-container">
-                  <span>{{ $t('home.send_btn') }}</span>
-                  <p></p>
-              </div>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ wallet.titleWallet }}</v-list-tile-title>
+              <v-list-tile-sub-title>{{ wallet.walletAddress }}</v-list-tile-sub-title>
+            </v-list-tile-content>
 
-          </md-list-item>
-        <md-list-item v-if="showFreeTokenButton" :href="freeAdmAddress" target="_blank">
-              <md-avatar class="md-avatar-icon">
-                <md-icon md-src="/img/Wallet/free-tokens.svg"></md-icon>
-              </md-avatar>
+            <v-list-tile-action>
+              <v-icon>mdi-content-copy</v-icon>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list>
 
-              <div class="md-list-text-container">
-                  <span>{{ $t('home.free_adm_btn') }} </span>
-                  <p></p>
-              </div>
-          </md-list-item>
+        <!-- Wallets Balance -->
+        <v-list two-line class="transparent">
+          <v-subheader>
+            Balance
+          </v-subheader>
+          <v-list-tile
+            v-for="wallet in wallets"
+            :key="wallet.system"
+            avatar
+            @click=""
+          >
+            <v-list-tile-avatar>
+              <v-icon class="grey lighten-1 white--text">{{ wallet.iconBalance }}</v-icon>
+            </v-list-tile-avatar>
 
-          <md-list-item :href="$t('home.invest_btn_link')+'?wallet='+this.$store.state.address" target="_blank" :title="$t('home.invest_btn_tooltip')">
+            <v-list-tile-content>
+              <v-list-tile-title>{{ wallet.titleBalance }}</v-list-tile-title>
+              <v-list-tile-sub-title>{{ wallet.walletBalance }} {{ wallet.system }}</v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
 
-              <md-avatar class="md-avatar-icon">
-                <md-icon md-src="/img/Wallet/join-ico.svg"></md-icon>
-              </md-avatar>
+        <!-- Actions -->
+        <v-list class="transparent">
 
-              <div class="md-list-text-container">
-                  <span>{{ $t('home.invest_btn') }} </span>
-                  <p></p>
-              </div>
-          </md-list-item>
-      </md-list>
-      </md-layout>
-      <md-snackbar md-position="bottom center" md-accent ref="homeSnackbar" md-duration="2000">
-          <span>{{ $t('home.copied') }}</span>
-      </md-snackbar>
-  </div>
+          <v-list-tile @click="sendFunds" avatar>
+            <v-list-tile-avatar>
+              <v-icon class="grey lighten-1 white--text">mdi-cube-send</v-icon>
+            </v-list-tile-avatar>
+
+            <v-list-tile-content>
+              <v-list-tile-title>Send funds</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile @click="buyTokens" avatar>
+            <v-list-tile-avatar>
+              <v-icon class="grey lighten-1 white--text">mdi-cash-usd</v-icon>
+            </v-list-tile-avatar>
+
+            <v-list-tile-content>
+              <v-list-tile-title>Buy tokens</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+
+        </v-list>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
+import { copyToClipboard } from '@/lib/textHelpers'
+
+
 export default {
-  name: 'home',
-  methods: {
-    copySuccess (e) {
-      this.$refs.homeSnackbar.open()
-    },
-    copy () {
-    },
-    goToTransactions (system) {
-      if (system === 'ADM') {
-        this.$router.push('/transactions/')
-      }
-    }
-  },
   computed: {
-    showFreeTokenButton () {
-      return this.$store.state.balance <= 0
-    },
-    freeAdmAddress () {
-      return 'https://adamant.im/free-adm-tokens/?wallet=' + this.$store.state.address
-    },
-    // isNewUser: function () {
-    //   return this.$store.state.is_new_account
-    // },
-    languageList: function () {
-      var messages = require('../i18n').default
-      return messages
-    },
-    wallets: function () {
+    wallets () {
       return [
         {
           system: 'ADM',
-          address: this.$store.state.address,
-          tooltip: 'home.your_address_tooltip_ADM',
-          balance: this.$store.state.balance,
-          balanceTooltip: 'home.your_balance_tooltip_ADM',
-          addressIcon: '/img/Wallet/adm-address.svg',
-          balanceIcon: '/img/Wallet/adm-balance.svg'
-        },
-        {
-          system: 'ETH',
-          address: this.$store.state.eth.address,
-          tooltip: 'home.your_address_tooltip_ETH',
-          balance: this.$store.state.eth.balance,
-          balanceTooltip: 'home.your_balance_tooltip_ETH',
-          addressIcon: '/img/Wallet/eth-address.svg',
-          balanceIcon: '/img/Wallet/eth-balance.svg'
+          walletAddress: this.$store.state.address,
+          walletBalance: this.$store.state.balance,
+          iconWallet: 'mdi-polaroid',
+          iconBalance: 'mdi-polaroid',
+          titleWallet: 'ADAMANT address',
+          titleBalance: 'ADAMANT balance'
         },
         {
           system: 'BNB',
-          address: this.$store.state.bnb.address,
-          tooltip: 'home.your_address_tooltip_BNB',
-          balance: this.$store.state.bnb.balance,
-          balanceTooltip: 'home.your_balance_tooltip_BNB',
-          addressIcon: '/img/Wallet/bnb-address.svg',
-          balanceIcon: '/img/Wallet/bnb-balance.svg'
+          walletAddress: this.$store.state.bnb.address,
+          walletBalance: this.$store.state.bnb.balance,
+          iconWallet: 'mdi-polaroid',
+          iconBalance: 'mdi-polaroid',
+          titleWallet: 'Binance Coin address',
+          titleBalance: 'Binance Coin balance'
+        },
+        {
+          system: 'ETH',
+          walletAddress: this.$store.state.eth.address,
+          walletBalance: this.$store.state.eth.balance,
+          iconWallet: 'mdi-ethereum',
+          iconBalance: 'mdi-ethereum',
+          titleWallet: 'Ethereum address',
+          titleBalance: 'Ethereum balance'
         }
       ]
     }
   },
-  watch: {
-    'language' (to, from) {
-      this.$i18n.locale = to
-    }
-  },
-  mounted () {
-    this.$store.commit('last_visited_chat', null)
-    this.isLogged = this.$store.getters.isLogged
-  },
-  data () {
-    return {
-      passPhrase: '',
-      language: 'en',
-      showCreate: false,
-      isLogged: false
+  data: () => ({
+  }),
+  methods: {
+    copyToClipboard (text) {
+      copyToClipboard(text)
+
+      this.$store.dispatch('snackbar/show', {
+        message: 'Copied'
+      })
+    },
+    sendFunds () {
+      this.$router.push('/transfer')
+    },
+    buyTokens () {
+      window.open('https://adamant.im/buy-tokens/?wallet=U9203183357885757380', '_blank')
     }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style>
-  .home .md-list {
-      max-width: 90%;
-  }
-  .for-address p {
-      text-overflow: ellipsis;
-      max-width: 100%;
-      display: inline-block;
-  }
-  .home .md-list-text-container p
-  {
-      font-style: italic;
-  }
-  .block_entry_title {
-    font-weight: 500;
-  }
-  .md-icon.custom-icon {
-    vertical-align: top !important;
-    width: 20px !important;
-    min-width: 20px !important;
-    height: 20px !important;
-    min-height: 20px !important;
-    font-size: 20px !important;
-  }
-</style>
