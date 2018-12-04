a5d2ea37320e7a8fa9d8573b17174f21c0508bf1
bluder <victorcpla@gmail.com>
2018-12-04 15:25:27 +0200

    fix(): disable i18n warnings when fallback to root

--- src/i18n.js
+++ src/i18n.js
@@ -19,5 +19,7 @@ function loadLocaleMessages () {
 export default new VueI18n({
   locale: process.env.VUE_APP_I18N_LOCALE || 'en',
   fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
-  messages: loadLocaleMessages()
+  messages: loadLocaleMessages(),
+  // fallbackRoot: true, // @todo why is not working?
+  silentTranslationWarn: true
 })
