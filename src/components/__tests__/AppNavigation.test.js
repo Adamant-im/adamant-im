a6a554f172d5ee15887ff75009a1217073bd0609
bluder <victorcpla@gmail.com>
2018-12-05 14:48:42 +0200

    test(AppNavigation): add unit tests

---
+++ src/components/__tests__/AppNavigation.test.js
@@ -0,0 +1,143 @@
+import { shallowMount } from '@vue/test-utils'
+import Vue from 'vue'
+import Vuex from 'vuex'
+import VueI18n from 'vue-i18n'
+import Vuetify from 'vuetify'
+
+import en from '@/i18n/en'
+import AppNavigation from '@/components/AppNavigation'
+
+Vue.use(Vuex)
+Vue.use(VueI18n)
+Vue.use(Vuetify)
+
+// @todo Update tests after refactor store
+describe('AppNavigation.vue', () => {
+  let mainStoreModule = null // used as reference to check jest.fn() calls
+  let store = null
+  let router = null
+  let i18n = null
+
+  beforeEach(() => {
+    // mockup Store
+    mainStoreModule = {
+      state: {
+        totalNewChats: 0
+      },
+      mutations: {
+        logout: jest.fn()
+      },
+      actions: {
+        reset: jest.fn()
+      }
+    }
+    store = new Vuex.Store(mainStoreModule)
+
+    // mockup i18n
+    i18n = new VueI18n({
+      locale: 'en',
+      fallbackLocale: 'en',
+      // fallbackRoot: true,
+      silentTranslationWarn: true, // @todo Replace with fallbackRoot: true after updating vue-i18n
+      messages: {
+        en
+      }
+    })
+  })
+
+  it('renders the correct markup', () => {
+    const wrapper = shallowMount(AppNavigation, {
+      store,
+      i18n,
+      mocks: {
+        $route: {
+          'path': '/home'
+        },
+        $router: {
+          push: jest.fn()
+        }
+      }
+    })
+
+    expect(wrapper.element).toMatchSnapshot()
+  })
+
+  it('verify vm.data', () => {
+    const wrapper = shallowMount(AppNavigation, {
+      store,
+      i18n,
+      mocks: {
+        $route: {
+          'path': '/home'
+        }
+      }
+    })
+    const vm = wrapper.vm
+
+    expect(Array.isArray(vm.pages)).toBe(true)
+    expect(vm.currentPageIndex).toBe(0)
+    expect(vm.showNav).toBe(true)
+  })
+
+  it('should cause mutations in store & call $router.push("/") when logout()', () => {
+    const wrapper = shallowMount(AppNavigation, {
+      store,
+      i18n,
+      mocks: {
+        $route: {
+          'path': '/home'
+        },
+        $router: {
+          push: jest.fn()
+        }
+      }
+    })
+
+    wrapper.vm.logout()
+
+    expect(mainStoreModule.mutations.logout).toHaveBeenCalled()
+    expect(mainStoreModule.actions.reset).toHaveBeenCalled()
+    expect(wrapper.vm.$router.push).toHaveBeenCalledWith('/')
+  })
+
+  it('should return indexOf current page depending on the $route.path', () => {
+    const wrapper = shallowMount(AppNavigation, {
+      store,
+      i18n,
+      mocks: {
+        $route: {
+          path: ''
+        }
+      }
+    })
+
+    wrapper.vm.$route.path = '/home'
+    expect(wrapper.vm.getCurrentPageIndex()).toBe(0)
+
+    wrapper.vm.$route.path = '/chats'
+    expect(wrapper.vm.getCurrentPageIndex()).toBe(1)
+
+    wrapper.vm.$route.path = '/options'
+    expect(wrapper.vm.getCurrentPageIndex()).toBe(2)
+  })
+
+  it('should display number of new messages', () => {
+    const wrapper = shallowMount(AppNavigation, {
+      store,
+      i18n,
+      mocks: {
+        $route: {
+          path: ''
+        }
+      }
+    })
+
+    // no messages
+    expect(wrapper.vm.numOfNewMessages).toBe(0)
+    expect(wrapper.find('vbadge-stub > span').exists()).toBe(false) // hidden badge
+
+    // new messages
+    mainStoreModule.state.totalNewChats = 10
+    expect(wrapper.find('vbadge-stub > span').text()).toBe('10') // visible badge
+  })
+})
