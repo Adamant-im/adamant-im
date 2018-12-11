6d7b787cef69788b1d81ab60c7f986075be4ed8b
bluder <victorcpla@gmail.com>
2018-12-11 12:28:23 +0200

    chore(uglify): remove uglify
    - return console.error in registerServiceWorker.js

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -3,5 +3,9 @@
 import { register } from 'register-service-worker'
 
 if (process.env.NODE_ENV === 'production') {
-  register(`${process.env.BASE_URL}service-worker.js`)
+  register(`${process.env.BASE_URL}service-worker.js`, {
+    error (error) {
+      console.error('Error during service worker registration:', error)
+    }
+  })
 }
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,3 @@
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const isProd = process.env.NODE_ENV === 'production'
-
 module.exports = {
   pluginOptions: {
 
@@ -11,16 +8,5 @@ module.exports = {
       enableInSFC: true
     }
 
-  },
-  chainWebpack: config => {
-    config.optimization.minimizer = isProd ? [
-      new UglifyJsPlugin({
-        uglifyOptions: {
-          compress: {
-            drop_console: true
-          }
-        }
-      })
-    ] : []
   }
 }
