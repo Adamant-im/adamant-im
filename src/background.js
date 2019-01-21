055891e4921bb7c67c6379c28939ca96ab5ff35f
Dmitrij Papulovskij <graycraft@pm.me>
2019-01-21 20:59:47 +0500

    Fix window resolution

--- src/background.js
+++ src/background.js
@@ -15,7 +15,7 @@ let win
 protocol.registerStandardSchemes(['app'], { secure: true })
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 })
+  win = new BrowserWindow({ width: 1200, height: 800 })
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
