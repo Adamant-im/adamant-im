3b4daa5b60958fd5b61cfb5867c0963305c21be9
Alexander Kiselev <alex.v.kiselev@gmail.com>
2019-01-24 02:01:52 +0300

    Don't check ADM balance before storing crypto address
    ADM will check it on server side anyway

--- src/store/index.js
+++ src/store/index.js
@@ -16,7 +16,7 @@ import delegatesModule from './modules/delegates'
 import nodesPlugin from './modules/nodes/nodes-plugin'
 
 import * as admApi from '../lib/adamant-api'
-import { base64regex, WelcomeMessage, UserPasswordHashSettings, Cryptos, Fees } from '../lib/constants'
+import { base64regex, WelcomeMessage, UserPasswordHashSettings, Cryptos } from '../lib/constants'
 import Queue from 'promise-queue'
 import utils from '../lib/adamant'
 import i18n from '../i18n'
@@ -411,7 +411,6 @@ const store = {
     },
     /** Stores user address for the specified crypto in the ADM KVS */
     storeCryptoAddress (context, { crypto, address }) {
-      if (context.state.balance < Fees.KVS) return
       return admApi.storeCryptoAddress(crypto, address)
     }
   },
