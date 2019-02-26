5ba4beffdfe91c4f44c0d89c1e72a1974d715fed
Dmitrij Papulovskij <graycraft@pm.me>
2019-02-26 16:18:03 +0500

    fix: trailing zeroes
    - cut trailing zeroes
    - cut remaining dot if number is integer
    - remove unnecessary dependency BigNumber
    - jsdoc

--- src/filters/numberFormat.js
+++ src/filters/numberFormat.js
@@ -1,12 +1,11 @@
-import BigNumber from '@/lib/bignumber'
-
 /**
- * Format currency amount to a fancy string with symbol
- * @param {number} amount - Amount to format
- * @param {number} precision - number of decimal digits
+ * Format number to a fancy string.
+ * Prevents exponential notation, unending fraction and trailing zeroes
+ * @param {number} amount Amount to format
+ * @param {number} precision Max fraction length
  * @returns {string}
  */
-export default (amount, precision = null) => {
-  const formatted = BigNumber(amount).toFixed(precision)
-  return `${formatted}`
+export default (amount, precision = 8) => {
+  let [whole, fraction] = Number(amount).toFixed(precision).replace(/0+$/, '').split('.')
+  return fraction ? `${whole}.${fraction}` : `${whole}`
 }
