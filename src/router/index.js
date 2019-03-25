0abec8c5383f084cdee431a46d23adb6275d3fbc
bludnic <victorcpla@gmail.com>
2019-03-25 21:29:35 +0200

    build(Login.vue): do not split Login.vue into separate chunk

--- src/router/index.js
+++ src/router/index.js
@@ -13,6 +13,7 @@ import Options from '@/views/Options'
 import Home from '@/views/Home'
 import Votes from '@/views/Votes'
 import Nodes from '@/views/Nodes'
+import Login from '@/views/Login'
 
 Vue.use(Router)
 
@@ -116,7 +117,7 @@ const router = new Router({
     {
       path: '/',
       name: 'Login',
-      component: () => import(/* webpackChunkName: "login" */ '@/views/Login.vue')
+      component: Login
     },
     {
       path: '*',
