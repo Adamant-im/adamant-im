c6e18aefa43bcded85f4cae3c0fa814a09064af9
bludnic <victorcpla@gmail.com>
2019-06-04 16:43:12 +0300

    fix(Date Filter): today & yesterday & currentWeek check

--- src/filters/date.js
+++ src/filters/date.js
@@ -1,18 +1,24 @@
 import dayjs from 'dayjs'
 import i18n from '@/i18n'
+import { isCurrentWeek, isToday, isYesterday } from './helpers'
 
 export default (timestamp) => {
   const date = dayjs(timestamp)
 
-  const diffDays = dayjs().diff(date, 'day')
-
-  if (diffDays < 1) {
+  if (isToday(new Date(timestamp))) {
     return date.format(`[${i18n.t('chats.date_today')}], HH:mm`)
-  } else if (diffDays < 2) {
+  } else if (isYesterday(new Date(timestamp))) {
     return date.format(`[${i18n.t('chats.date_yesterday')}], HH:mm`)
-  } else if (diffDays <= 7) {
-    return date.format('ddd, HH:mm')
+  } else if (isCurrentWeek(new Date(timestamp))) {
+    return date.format('dddd, HH:mm')
   } else {
-    return date.format('MMM DD, YYYY, HH:mm')
+    const currentYear = (new Date()).getFullYear()
+    const messageYear = (new Date(timestamp)).getFullYear()
+
+    if (currentYear === messageYear) {
+      return date.format('MMM DD')
+    } else {
+      return date.format('MMM DD, YYYY')
+    }
   }
 }
--- src/filters/dateBrief.js
+++ src/filters/dateBrief.js
@@ -1,15 +1,16 @@
 import dayjs from 'dayjs'
 import i18n from '@/i18n'
+import { isToday, isYesterday, isCurrentWeek } from './helpers'
 
 export default (timestamp) => {
   const date = dayjs(timestamp)
 
-  const diffDays = dayjs().diff(date, 'day')
-
-  if (diffDays < 1) {
+  if (isToday(new Date(timestamp))) {
     return date.format(`[${i18n.t('chats.date_today')}], HH:mm`)
-  } else if (diffDays < 2) {
+  } else if (isYesterday(new Date(timestamp))) {
     return date.format(`[${i18n.t('chats.date_yesterday')}], HH:mm`)
+  } else if (isCurrentWeek(new Date(timestamp))) {
+    return date.format('dddd')
   } else {
     const currentYear = (new Date()).getFullYear()
     const messageYear = (new Date(timestamp)).getFullYear()
