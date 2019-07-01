4751cf6c11f7e83ed232b84f3d9d3a90f6937260
bludnic <victorcpla@gmail.com>
2019-07-01 23:53:15 +0300

    feat(URI): add generateURI method

--- src/lib/uri.js
+++ src/lib/uri.js
@@ -39,3 +39,14 @@ export function parseURI (str) {
     return data
   }
 }
+
+/**
+ * Generate ADAMANT URI address.
+ *
+ * @param {string} address ADAMANT address
+ * @param {string} name Contact name
+ * @returns {string}
+ */
+export function generateURI (address, name) {
+  return `adm:${address}${name ? '?label=' + name : ''}`
+}
