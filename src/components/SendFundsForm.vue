2ae510982841b313b41454ea37b7260a1a7071c0
Dmitrij Papulovskij <graycraft@pm.me>
2019-08-22 23:19:27 +0500

    feat(SendFundsForm): QR code and amount divider
    ADAMANTPWA-421
    - Drop menu in recipient field for decode address from camera or an image file.
    - Drop menu in amount field for dividing amount to send.
    - Hide spin buttons for amount field.

--- src/components/SendFundsForm.vue
+++ src/components/SendFundsForm.vue
@@ -27,6 +27,23 @@
             {{ $t('transfer.to_address_label') }}
           </span>
         </template>
+        <template slot="append">
+          <v-menu :offset-overflow="true" :offset-y="false" left>
+            <v-icon slot="activator">mdi-dots-vertical</v-icon>
+            <v-list>
+              <v-list-tile @click="showQrcodeScanner = true">
+                <v-list-tile-title>{{ $t('transfer.decode_from_camera') }}</v-list-tile-title>
+              </v-list-tile>
+              <v-list-tile class="v-list__tile--link">
+                <v-list-tile-title>
+                  <qrcode-capture @detect="onDetectQrcode" @error="onDetectQrcodeError">
+                    <span>{{ $t('transfer.decode_from_image') }}</span>
+                  </qrcode-capture>
+                </v-list-tile-title>
+              </v-list-tile>
+            </v-list>
+          </v-menu>
+        </template>
       </v-text-field>
 
       <v-text-field
@@ -42,6 +59,20 @@
           <span class="body-1">
             {{ `(max: ${maxToTransferFixed} ${currency})` }}
           </span>
+        </template>
+        <template slot="append">
+          <v-menu :offset-overflow="true" :offset-y="false" left>
+            <v-icon slot="activator">mdi-dots-vertical</v-icon>
+            <v-list>
+              <v-list-tile
+                :key="item.title"
+                @click="divideAmount(item.divider)"
+                v-for="item in amountMenuItems"
+              >
+                <v-list-tile-title>{{ $t(item.title) }}</v-list-tile-title>
+              </v-list-tile>
+            </v-list>
+          </v-menu>
         </template>
       </v-text-field>
 
@@ -120,10 +151,18 @@
         </v-card-actions>
       </v-card>
     </v-dialog>
+
+    <qrcode-scanner-dialog
+      @scan="onScanQrcode"
+      v-if="showQrcodeScanner"
+      v-model="showQrcodeScanner"
+    />
   </div>
 </template>
 
 <script>
+import QrcodeCapture from '@/components/QrcodeCapture'
+import QrcodeScannerDialog from '@/components/QrcodeScannerDialog'
 import get from 'lodash/get'
 import { BigNumber } from 'bignumber.js'
 
@@ -154,6 +193,10 @@ function validateForm () {
 }
 
 export default {
+  components: {
+    QrcodeCapture,
+    QrcodeScannerDialog
+  },
   created () {
     this.currency = this.cryptoCurrency
     this.address = this.recipientAddress
@@ -321,10 +364,28 @@ export default {
     cryptoAddress: '',
     amountString: '',
     amount: 0,
+    amountMenuItems: [
+      {
+        divider: 10,
+        title: 'transfer.amount_percent_10'
+      },
+      {
+        divider: 3,
+        title: 'transfer.amount_percent_33'
+      },
+      {
+        divider: 2,
+        title: 'transfer.amount_percent_50'
+      },
+      {
+        divider: 1,
+        title: 'transfer.amount_percent_100'
+      }
+    ],
     comment: '',
-
     validForm: true,
     disabledButton: false,
+    showQrcodeScanner: false,
     showSpinner: false,
     dialog: false,
     fetchAddress: null // fn throttle
@@ -340,6 +401,41 @@ export default {
           message: abstract,
           timeout: 3000
         })
+      }
+    },
+
+    /**
+     * Handle successful address decode from a QR code
+     * @param {string} address Address
+     */
+    onDetectQrcode (address) {
+      this.onScanQrcode(address)
+    },
+
+    /**
+     * Handle failed address decode from a QR code
+     * @param {string} error Error instance
+     */
+    onDetectQrcodeError (error) {
+      this.cryptoAddress = ''
+      this.$store.dispatch('snackbar/show', {
+        message: this.$t('transfer.invalid_qr_code')
+      })
+      console.warn(error)
+    },
+
+    /**
+     * Validate address from a QR code
+     * @param {string} address Address
+     */
+    onScanQrcode (address) {
+      const message = this.validationRules.cryptoAddress
+        .map(v => v(address))
+        .filter(v => v !== true)[0]
+      if (message) {
+        this.$store.dispatch('snackbar/show', { message })
+      } else {
+        this.cryptoAddress = address
       }
     },
     submit () {
@@ -407,6 +503,14 @@ export default {
           comments: this.comment
         })
       }
+    },
+
+    /**
+     * Divide amount by predefined value
+     * @param {number} divider How much less to send
+     */
+    divideAmount (divider) {
+      this.amountString = BigNumber(this.maxToTransfer / divider).toFixed()
     },
     pushTransactionToChat (transactionId, adamantAddress) {
       let amount = this.amount
@@ -479,6 +583,11 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+.a-input >>> input[type=number]
+  -moz-appearance: textfield
+.a-input >>> input[type=number]::-webkit-inner-spin-button,
+.a-input >>> input[type=number]::-webkit-outer-spin-button
+  -webkit-appearance: none
 .send-funds-form
   &__button
     margin-top: 15px
