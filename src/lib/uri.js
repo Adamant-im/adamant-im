288b92b60dec6581c3823a94dd7187827a29aec3
Dmitrij Papulovskij <graycraft@pm.me>
2019-08-30 18:47:29 +0500

    feat(lib/uri.js): AIP compliency and cryptos support
    ADAMANTPWA-423
    - Parse info from an URI containing any supported cryptocurrency address.
    - Make URI parser complient with AIP-8 and AIP-9.

--- src/lib/uri.js
+++ src/lib/uri.js
@@ -1,43 +1,55 @@
-export const pattern = /^adm:(U[0-9]{6,})(\?(\w+(=[^?&=]*)?(&\w+(=[^?&=]*)?)*)?)?$/i
+import { Cryptos, RE_DASH_ADDRESS, RE_DOGE_ADDRESS } from './constants'
 
 /**
- * AIP 2: URI Format for ADAMANT
- *
- * @param {string} str
+ * Parse info from an URI containing a cryptocurrency address
+ * Complies with AIP-2, AIP-8, AIP-9
+ * @param {string} uri URI
  * @returns {
  *   {
  *     address: string,
- *     params: Map<string, string>
- *   } | undefined
+ *     crypto: string,
+ *     params: Object<string, string>,
+       protocol: string
+ *   }
  * }
  */
-export function parseURI (str) {
-  const match = str.match(pattern)
+export function parseURI (uri) {
+  const [origin, query = ''] = uri.split('?')
+  let address = ''
+  let crypto = ''
+  let params = Object.create(null)
+  let protocol = ''
 
-  if (match) {
-    let data = {
-      address: match[1],
-      params: {}
-    }
+  if (query) {
+    params = query.split('&').reduce((accum, param) => {
+      const [key, value = ''] = param.split('=')
 
-    if (match[2]) {
-      data.params = match[2]
-        .slice(1) // remove ? symbol
-        .split('&')
-        .reduce((acc, keyValue) => {
-          const [key, value] = keyValue.split('=')
-
-          if (key === '') return acc
-
-          return {
-            ...acc,
-            [key]: value !== undefined ? decodeURI(value) : value
-          }
-        }, {})
-    }
-
-    return data
+      return key && value ? {
+        ...accum,
+        [key]: window.decodeURIComponent(value)
+      } : accum
+    }, Object.create(null))
   }
+  if (origin.includes(':')) {
+    [protocol, address] = origin.split(':')
+    if (protocol === 'ethereum') {
+      crypto = Cryptos.ETH
+    } else if (protocol === 'https' && /^\/\/msg.adamant.im$/.test(address)) {
+      crypto = Cryptos.ADM
+      address = params.address; delete params.address
+    } else if (Cryptos.hasOwnProperty(protocol.toUpperCase())) {
+      crypto = protocol.toUpperCase()
+    }
+  } else {
+    address = origin
+    if (RE_DASH_ADDRESS.test(address)) {
+      crypto = Cryptos.DASH
+    } else if (RE_DOGE_ADDRESS.test(address)) {
+      crypto = Cryptos.DOGE
+    }
+  }
+
+  return { address, crypto, params, protocol }
 }
 
 /**
