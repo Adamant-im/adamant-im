12bf275c09769cafc2bc78eeaf8916f069ac2f7a
Dmitrij Papulovskij <graycraft@pm.me>
2019-09-03 00:33:58 +0500

    feat(constants.js): Bitcoin and Lisk
    ADAMANTPWA-433
    - Bitcoin and Lisk address RegExps.

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -87,8 +87,10 @@ export const Fees = {
 export const base64regex = /^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$/
 
 export const RE_ADM_ADDRESS = /^U([0-9]{6,})$/i
+export const RE_BTC_ADDRESS = /^(bc1|[13])[a-km-zA-HJ-NP-Z02-9]{25,39}$/
 export const RE_DASH_ADDRESS = /^[7X][1-9A-HJ-NP-Za-km-z]{33,}$/
 export const RE_DOGE_ADDRESS = /^[A|D|9][A-Z0-9]([0-9a-zA-Z]{9,})$/
+export const RE_LISK_ADDRESS = /^[0-9]{2,21}L$/
 
 export const Symbols = {
   CLOCK: String.fromCharCode(0x23f0), // ⏰
