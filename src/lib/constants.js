88fe9026f96c357be0f216200097708c9501d3f9
Dmitrij Papulovskij <graycraft@pm.me>
2019-09-03 00:32:19 +0500

    fix(constants.js): Dash address RegExp
    ADAMANTPWA-433
    - Change Dash address RegExp to support multisignature P2SH addresses.

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -87,7 +87,7 @@ export const Fees = {
 export const base64regex = /^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$/
 
 export const RE_ADM_ADDRESS = /^U([0-9]{6,})$/i
-export const RE_DASH_ADDRESS = /^X[1-9A-HJ-NP-Za-km-z]{33,}$/
+export const RE_DASH_ADDRESS = /^[7X][1-9A-HJ-NP-Za-km-z]{33,}$/
 export const RE_DOGE_ADDRESS = /^[A|D|9][A-Z0-9]([0-9a-zA-Z]{9,})$/
 
 export const Symbols = {
