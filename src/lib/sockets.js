e3fc6288f1aef45794e7784eeaf8149f46fd02f7
bludnic <victorcpla@gmail.com>
2019-09-23 18:20:38 +0300

    refactor(sockets): refactor SocketClient and SocketPlugin
    * Implement EventEmitter
    
    * Revise connection every 5 sec
    
    * Handle node status change, toggle node, use fastest node
    
    * Handle useSocketConnection option

--- src/lib/sockets.js
+++ src/lib/sockets.js
@@ -1,53 +1,222 @@
 import io from 'socket.io-client'
 import random from 'lodash/random'
 
-import config from '@/config'
-
-export class SocketClient {
+/**
+ * interface Events {
+ *   [key: string]: Function[];
+ * }
+ */
+export class EventEmitter {
   /**
-   * @param {string[]} nodes Array of nodes URLs
+   * @param {Events} events
    */
-  constructor (nodes) {
-    this.nodes = nodes
+  constructor (events) {
+    this.events = events || {}
   }
+
+  /**
+   * @param {string} event
+   * @param {Function} cb
+   */
+  subscribe (event, cb) {
+    (this.events[event] || (this.events[event] = [])).push(cb)
+
+    return {
+      unsubscribe: () =>
+        this.events[event] && this.events[event].splice(this.events[event].indexOf(cb) >>> 0, 1)
+    }
+  }
+
+  /**
+   * @param {string} event
+   * @param {any[]} args
+   */
+  emit (event, ...args) {
+    (this.events[event] || []).forEach(fn => fn(...args))
+  }
+}
+
+export class SocketClient extends EventEmitter {
+  adamantAddress = ''
+  nodes = {}
+
+  /**
+   * The current node to which we are connected
+   * @type {string}
+   */
+  currentSocketAddress = ''
+
+  /**
+   * Set true when chat messages are loaded
+   * @type {boolean}
+   */
+  isSocketReady = false
+
+  /**
+   * Sync with store.state.options.useSocketConnection
+   * @type {boolean}
+   */
+  isSocketEnabled = false
+
+  /**
+   * Sync with store.state.nodes.useFastest
+   * @type {boolean}
+   */
+  useFastest = false
+
+  interval = null
+
+  /**
+   * @type {number}
+   */
+  REVISE_CONNECTION_TIMEOUT = 5000
 
   /**
    * Get random socket address.
    * @returns {string}
    */
   get socketAddress () {
-    return this.nodes[random(this.nodes.length - 1)]
+    const node = this.useFastest
+      ? this.fastestNode
+      : this.randomNode
+
+    return node.url.replace(/^https?:\/\/(.*)$/, '$1')
   }
 
-  on (event, fn) {
-    this.connection.on(event, fn)
+  get fastestNode () {
+    return this.nodes.reduce((fastest, current) => {
+      if (!current.online || !current.active || current.outOfSync) {
+        return fastest
+      }
+      return (!fastest || fastest.ping > current.ping) ? current : fastest
+    })
   }
 
-  connect (adamantAddress) {
-    this.connection = io(this.socketAddress, { reconnection: false, timeout: 5000 })
+  get randomNode () {
+    const activeNodes = this.nodes.filter(n => n.online && n.active && !n.outOfSync && n.socketSupport)
+
+    return activeNodes[random(activeNodes.length - 1)]
+  }
+
+  get hasActiveNodes () {
+    return Object.values(this.nodes)
+      .some(n => n.online && n.active && !n.outOfSync && n.socketSupport)
+  }
+
+  get isOnline () {
+    return this.connection && this.connection.connected
+  }
+
+  get isCurrentNodeActive () {
+    return this.nodes.some(
+      node => node.url.match(new RegExp(this.currentSocketAddress)) && node.active
+    )
+  }
+
+  /**
+   * Update nodes statuses.
+   * @param nodes
+   */
+  setNodes (nodes) {
+    this.nodes = nodes
+  }
+
+  setAdamantAddress (address) {
+    this.adamantAddress = address
+  }
+
+  setSocketReady (value) {
+    this.isSocketReady = value
+  }
+
+  setSocketEnabled (value) {
+    this.isSocketEnabled = value
+
+    if (!value) this.disconnect()
+  }
+
+  /**
+   * @param {boolean} value
+   */
+  setUseFastest (value) {
+    this.useFastest = value
+  }
+
+  /**
+   * Subscribe to socket events.
+   */
+  subscribeToEvents () {
+    if (this.connection) {
+      this.connection.on('newTrans', transaction => {
+        if (transaction.type === 0 || transaction.type === 8) this.emit('newMessage', transaction)
+      })
+    }
+  }
+
+  /**
+   * @param address ADAMANT address
+   */
+  init (address) {
+    this.setAdamantAddress(address)
+    this.setSocketReady(true)
+    this.interval = setInterval(() => this.reviseConnection(), this.REVISE_CONNECTION_TIMEOUT)
+  }
+
+  destroy () {
+    clearInterval(this.interval)
+    this.setSocketReady(false)
+    this.disconnect()
+  }
+
+  connect (socketAddress) {
+    this.connection = io(`wss://${socketAddress}`, { reconnection: false, timeout: 5000 })
 
     this.connection.on('connect', () => {
-      this.connection.emit('msg', adamantAddress + ' connected!')
-      this.connection.emit('address', adamantAddress)
+      this.currentSocketAddress = socketAddress
+      this.connection.emit('msg', this.adamantAddress + ' connected!')
+      this.connection.emit('address', this.adamantAddress)
     })
 
     this.connection.on('disconnect', reason => {
       if (reason === 'ping timeout' || reason === 'io server disconnect') {
-        this.connect(adamantAddress)
+        console.warn('[Socket] Disconnected. Reason:', reason)
       }
     })
 
     this.connection.on('connect_error', (err) => {
-      console.warn('connect_error', err)
-      setTimeout(() => this.connect(adamantAddress), 5000)
+      console.warn('[Socket] connect_error', err)
     })
   }
 
   disconnect () {
     this.connection && this.connection.close()
   }
+
+  reviseConnection () {
+    if (!this.isSocketReady) return
+    if (!this.isSocketEnabled) return
+    if (!this.hasActiveNodes) {
+      this.disconnect()
+      console.warn('[Socket]: No active nodes')
+      return
+    }
+
+    const socketAddress = this.socketAddress
+
+    if (
+      (
+        this.isOnline &&
+        this.useFastest &&
+        this.currentSocketAddress !== socketAddress
+      ) ||
+      !this.isOnline ||
+      !this.isCurrentNodeActive
+    ) {
+      this.disconnect()
+      this.connect(socketAddress)
+      this.subscribeToEvents()
+    }
+  }
 }
 
-const nodes = config.server.adm.map(node => node.url.replace(/^https?:\/\/(.*)$/, 'wss://$1'))
-
-export default new SocketClient(nodes)
+export default new SocketClient()
--- src/store/plugins/socketsPlugin.js
+++ src/store/plugins/socketsPlugin.js
@@ -1,14 +1,8 @@
 import socketClient from '@/lib/sockets'
 import { decodeChat, getPublicKey } from '@/lib/adamant-api'
 
-function openSocketConnection (adamantAddress, store) {
-  if (!store.state.options.useSocketConnection) return
-  if (socketClient.connected) return
-
-  socketClient.connect(adamantAddress)
-  socketClient.on('newTrans', transaction => {
-    if (transaction.type !== 0 && transaction.type !== 8) return
-
+function subscribe (store) {
+  socketClient.subscribe('newMessage', transaction => {
     const promise = (transaction.recipientId === store.state.address)
       ? Promise.resolve(transaction.senderPublicKey)
       : getPublicKey(transaction.recipientId)
@@ -23,23 +17,38 @@ function openSocketConnection (adamantAddress, store) {
 }
 
 export default store => {
+  subscribe(store)
+
+  socketClient.setSocketEnabled(store.state.options.useSocketConnection)
+
   // open socket connection when chats are loaded
-  store.watch(() => store.state.chat.isFulfilled, value => {
-    if (value) openSocketConnection(store.state.address, store)
+  store.watch(() => store.state.chat.isFulfilled, isFulfilled => {
+    if (isFulfilled) socketClient.init(store.state.address)
   })
 
   // when logout or update `useSocketConnection` option
-  store.subscribe((mutation, state) => {
-    if (mutation.type === 'reset') {
-      socketClient.disconnect()
+  store.subscribe(mutation => {
+    if (mutation.type === 'reset') socketClient.destroy()
+
+    if (
+      mutation.type === 'options/updateOption' &&
+      mutation.payload.key === 'useSocketConnection'
+    ) {
+      socketClient.setSocketEnabled(mutation.payload.value)
+    }
+  })
+
+  // when statusUpdate/enable/disable/useFastest node
+  store.subscribe(mutation => {
+    if (
+      mutation.type === 'nodes/status' ||
+      mutation.type === 'nodes/toggle'
+    ) {
+      socketClient.setNodes(store.getters['nodes/list'])
     }
 
-    if (mutation.type === 'options/updateOption' && mutation.payload.key === 'useSocketConnection') {
-      if (mutation.payload.value) {
-        openSocketConnection(state.address, store)
-      } else {
-        socketClient.disconnect()
-      }
+    if (mutation.type === 'nodes/useFastest') {
+      socketClient.setUseFastest(mutation.payload)
     }
   })
 }
