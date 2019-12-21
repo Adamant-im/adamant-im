251c0cd99c6502b3398daa8eaa7c9da68c2f0aff
Alexander Kiselev <alex.v.kiselev@gmail.com>
2019-12-21 22:20:20 +0300

    Comments

--- src/lib/bitcoin/bitcoin-utils.js
+++ src/lib/bitcoin/bitcoin-utils.js
@@ -1,5 +1,10 @@
 import bitcoin from 'bitcoinjs-lib'
 
+/**
+ * Checks if the supplied string is a valid BTC address
+ * @param {string} address address to check
+ * @returns {boolean}
+ */
 export function isValidAddress (address) {
   try {
     bitcoin.address.toOutputScript(address)
