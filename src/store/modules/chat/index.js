ed54d01248949e611c64728b78a5cdf9308230a8
Alexander Kiselev <alex.v.kiselev@gmail.com>
2020-01-27 22:19:11 +0300

    ADAMANT Bounty chat fix

--- src/store/modules/chat/index.js
+++ src/store/modules/chat/index.js
@@ -407,7 +407,7 @@ const mutations = {
       }
     ]
 
-    Vue.set(state.chats, 'U15423595369615486571', {
+    Vue.set(state.chats, 'chats.virtual.welcome_message_title', {
       messages: bountyMessages,
       numOfNewMessages: 0,
       isAdamantChat: true,
