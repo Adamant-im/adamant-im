d6123e04e6a9e33efddf885733b30ad52ca5ca9b
Aleksei Lebedev <msg@adamant.im>
2020-06-20 19:24:47 +0300

    Add How to use messenger link

--- src/components/FreeTokensDialog.vue
+++ src/components/FreeTokensDialog.vue
@@ -6,14 +6,14 @@
   >
     <v-card>
       <v-card-title class="a-text-header">
-        {{ $t("home.free_adm_title") }}
+        {{ $t("chats.free_adm_title") }}
       </v-card-title>
 
       <v-divider class="a-divider"></v-divider>
 
       <v-card-text>
         <div :class="`${className}__disclaimer a-text-regular-enlarged`">
-          {{ $t("home.free_adm_disclaimer") }}
+          {{ $t("chats.free_adm_disclaimer") }}
         </div>
       </v-card-text>
 
@@ -25,6 +25,13 @@
           <div :class="`${className}__btn-text`">{{ $t('home.free_adm_btn') }}</div>
         </v-btn>
       </v-flex>
+
+      <v-flex xs12 :class="`${className}__btn-show-article`">
+        <a @click="showArticle()" class="a-text-active">
+          {{ $t('chats.how_to_use_messenger') }}
+        </a>
+      </v-flex>
+
     </v-card>
   </v-dialog>
 </template>
@@ -47,6 +54,10 @@ export default {
       const link = 'https://adamant.im/free-adm-tokens/?wallet=' + this.$store.state.address
       window.open(link, '_blank', 'resizable,scrollbars,status,noopener')
       this.show = false
+    },
+    showArticle () {
+      const link = this.$t('chats.how_to_use_messenger_link')
+      window.open(link, '_blank', 'resizable,scrollbars,status,noopener')
     }
   },
   props: {
@@ -59,11 +70,14 @@ export default {
 </script>
 <style lang="stylus" scoped>
   .free-tokens-dialog
-    $__disclaimer
+    &__disclaimer
       margin-top: 10px
     &__btn-free-tokens
-      margin-top: 10px
-      margin-bottom: 30px
+      margin-top: 15px
+      margin-bottom: 25px
     &__btn-icon
       margin-right: 8px
+    &__btn-show-article
+      padding-bottom: 30px
+      text-align: center
 </style>
