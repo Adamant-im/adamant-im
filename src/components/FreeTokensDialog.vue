632d1612aae87a5b6fd56459581567b0d697ab09
Aleksei Lebedev <msg@adamant.im>
2020-06-20 18:49:28 +0300

    Update styles

--- src/components/FreeTokensDialog.vue
+++ src/components/FreeTokensDialog.vue
@@ -8,23 +8,23 @@
       <v-card-title class="a-text-header">
         {{ $t("home.free_adm_title") }}
       </v-card-title>
+
+      <v-divider class="a-divider"></v-divider>
+
       <v-card-text>
         <div :class="`${className}__disclaimer a-text-regular-enlarged`">
           {{ $t("home.free_adm_disclaimer") }}
         </div>
       </v-card-text>
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn
-          flat="flat"
-          class="a-btn-regular"
+
+      <v-flex xs12 class="text-xs-center">
+        <v-btn :class="[`${className}__btn-free-tokens`, 'a-btn-primary']"
           @click="getFreeTokens()"
         >
           <v-icon :class="`${className}__btn-icon`">mdi-gift</v-icon>
           <div :class="`${className}__btn-text`">{{ $t('home.free_adm_btn') }}</div>
         </v-btn>
-        <v-spacer></v-spacer>
-      </v-card-actions>
+      </v-flex>
     </v-card>
   </v-dialog>
 </template>
@@ -59,6 +59,11 @@ export default {
 </script>
 <style lang="stylus" scoped>
   .free-tokens-dialog
+    $__disclaimer
+      margin-top: 10px
+    &__btn-free-tokens
+      margin-top: 10px
+      margin-bottom: 30px
     &__btn-icon
       margin-right: 8px
 </style>
