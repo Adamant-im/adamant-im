632022278cfb9951466795f0e68aeadfd3a881b4
Alexander Kiselev <alex.v.kiselev@gmail.com>
2020-06-25 19:50:23 +0300

    Keys export logic

--- src/components/ExportKeysForm.vue
+++ src/components/ExportKeysForm.vue
@@ -1,6 +1,41 @@
 <template>
   <v-form>
-    <div>
+    <div v-if="keys.length" :class="`${className}__keys`">
+      <div
+        v-for="key in keys"
+        :key="key.crypto"
+      >
+        <v-text-field
+          v-model="key.key"
+          :readonly="true"
+          class="a-input"
+          type="text"
+        >
+          <template slot="label">
+            <span class="font-weight-medium">
+              {{ key.cryptoName }}
+            </span>
+          </template>
+          <template slot="append">
+            <v-btn icon ripple :class="`${className}__btn-copy`" @click="copyKey(key.key)">
+              <v-icon :class="`${className}__icon`" size="20">mdi-content-copy</v-icon>
+            </v-btn>
+          </template>
+        </v-text-field>
+      </div>
+
+      <div class="text-xs-center">
+        <v-btn
+          :class="`${className}__button`"
+          @click="copyAll"
+          class="a-btn-primary"
+        >
+          {{ $t('options.export_keys.copy_all') }}
+        </v-btn>
+      </div>
+    </div>
+
+    <div :class="`${className}__disclaimer`">
       {{ $t("options.export_keys.disclaimer") }}
     </div>
 
@@ -36,7 +71,7 @@
     <div class="text-xs-center">
       <v-btn
         :class="`${className}__button`"
-        @click="confirm"
+        @click="revealKeys"
         class="a-btn-primary"
       >
         {{ $t('options.export_keys.button') }}
@@ -51,8 +86,27 @@
   </v-form>
 </template>
 <script>
+import { validateMnemonic } from 'bip39'
+import { getAccountFromPassphrase as getEthAccount } from '@/lib/eth-utils'
+import { getAccount as getBtcAccount } from '@/lib/bitcoin/btc-base-api'
+import { Cryptos, CryptosNames } from '@/lib/constants'
+import { copyToClipboard } from '@/lib/textHelpers'
+
 import QrcodeCapture from '@/components/QrcodeCapture'
 import QrcodeScannerDialog from '@/components/QrcodeScannerDialog'
+
+function getBtcKey (crypto, passphrase, asWif) {
+  const keyPair = getBtcAccount(crypto, passphrase).keyPair
+  const key = asWif
+    ? keyPair.toWIF()
+    : `0x${keyPair.privateKey.toString('hex')}`
+
+  return {
+    crypto: crypto,
+    cryptoName: CryptosNames[crypto],
+    key
+  }
+}
 
 export default {
   components: {
@@ -62,7 +116,8 @@ export default {
   data () {
     return {
       passphrase: '',
-      showQrcodeScanner: false
+      showQrcodeScanner: false,
+      keys: []
     }
   },
   computed: {
@@ -83,7 +138,55 @@ export default {
 
     onScanQrcode (pass) {
       this.passphrase = pass
+    },
+
+    revealKeys () {
+      this.keys = []
+
+      if (!validateMnemonic(this.passphrase)) {
+        this.$store.dispatch('snackbar/show', {
+          message: this.$t('login.invalid_passphrase')
+        })
+        return
+      }
+
+      setTimeout(() => {
+        const eth = {
+          crypto: Cryptos.ETH,
+          cryptoName: this.$t('options.export_keys.eth'),
+          key: getEthAccount(this.passphrase).privateKey
+        }
+
+        const bitcoin = getBtcKey(Cryptos.BTC, this.passphrase, true)
+        const dash = getBtcKey(Cryptos.DASH, this.passphrase, false)
+        const doge = getBtcKey(Cryptos.DOGE, this.passphrase, true)
+
+        this.keys = [bitcoin, eth, doge, dash]
+      }, 100)
+    },
+
+    copyKey (key) {
+      copyToClipboard(key)
+      this.$store.dispatch('snackbar/show', {
+        message: this.$t('home.copied'),
+        timeout: 1500
+      })
+    },
+
+    copyAll () {
+      const allKeys = this.keys.map(k => `${k.cryptoName}\r\n${k.key}`).join('\r\n\r\n')
+      this.copyKey(allKeys)
     }
   }
 }
 </script>
+<style lang="stylus" scoped>
+  .export-keys-form
+    &__keys
+      margin-bottom: 24px
+    &__disclaimer
+      margin-bottom: 24px
+    &__btn-copy
+      margin-right: 0
+      margin-bottom: 0
+</style>
