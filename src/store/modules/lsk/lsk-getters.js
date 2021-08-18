3db93f76eee1495cd362551c28a0f77d9ec70030
adamant-al <adamantim@yandex.ru>
2021-08-18 12:15:07 +0300

    Fix LSK fee calculation

--- src/store/modules/lsk/lsk-getters.js
+++ src/store/modules/lsk/lsk-getters.js
@@ -1,11 +1,18 @@
 import baseGetters from '../lsk-base/lsk-base-getters'
 import lskActions from './lsk-actions'
+import validateAddress from '@/lib/validateAddress'
 
 export default {
   ...baseGetters,
 
   fee: state => (amount, recipientAddress, data) => {
-    return lskActions.calculateFee(null, { address: recipientAddress || state.address, amount, nonce: state.nonce, data: data || '' })
+    recipientAddress = validateAddress(state.crypto, recipientAddress) ? recipientAddress : state.address
+    return lskActions.calculateFee(null, {
+      address: recipientAddress,
+      amount,
+      nonce: state.nonce,
+      data: data || ''
+    })
   },
 
   height (state) {
