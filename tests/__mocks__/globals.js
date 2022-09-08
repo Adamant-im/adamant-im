6d9665ba84107a5b04d5d5b9bb00b50e9245673d
bludnic <victorcpla@gmail.com>
2022-09-08 20:49:01 +0100

    fix(tests): fix `SyntaxError: Cannot use import statement outside a module`

--- tests/__mocks__/globals.js
+++ tests/__mocks__/globals.js
@@ -1,1 +1,5 @@
 require('./indexedDB')
+
+// This is a workaround to fix the tests that have the `vueBus` as an import.
+// The functionality of `vueBus` should be rethinked or removed in the future.
+jest.mock('@/main', () => {})
