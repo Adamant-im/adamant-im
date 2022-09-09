c4d10c84d4057e39e4b85e2624b4afa52219dab0
bludnic <victorcpla@gmail.com>
2022-09-09 16:36:51 +0100

    feat(mocks, globals, vueBus): add a comment about Vue 3 migration

--- tests/__mocks__/globals.js
+++ tests/__mocks__/globals.js
@@ -2,4 +2,7 @@ require('./indexedDB')
 
 // This is a workaround to fix the tests that have the `vueBus` as an import.
 // The functionality of `vueBus` should be rethinked or removed in the future.
+//
+// $on, $off and $once instance methods will be removed in Vue 3
+// @see https://v3-migration.vuejs.org/breaking-changes/events-api.html
 jest.mock('@/main', () => {})
