1f212f3a1cde3f146c45db984918e009c4f7926f
bludnic <victorcpla@gmail.com>
2022-10-17 23:08:04 +0100

    fix(vuetify2, Home, Tabs): fix arrows paddings

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -235,12 +235,16 @@ export default {
       top: 0;
       bottom: 0;
       left: 0;
+      flex-basis: 32px;
+      min-width: 32px;
     }
     :deep(.v-slide-group__next) {
       position: absolute;
       top: 0;
       bottom: 0;
       right: 0;
+      flex-basis: 32px;
+      min-width: 32px;
     }
   }
   &__icon {
