66e6ef8243a592e46fedfd73d21ce4e9a9ecf3bd
bludnic <victorcpla@gmail.com>
2022-11-11 20:38:16 +0000

    migration(vue-i18n, navigatorOnline): fix undefined `t()`

--- src/store/plugins/navigatorOnline.js
+++ src/store/plugins/navigatorOnline.js
@@ -6,7 +6,7 @@ export default store => {
 
   function handleEvent (event) {
     store.dispatch('snackbar/show', {
-      message: i18n.t(`connection.${event.type}`),
+      message: i18n.global.t(`connection.${event.type}`),
       timeout: 3000
     })
   }
