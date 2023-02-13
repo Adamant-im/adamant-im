370c11d97f5608cedeaad6f1c4c19c5573867dfc
bludnic <victorcpla@gmail.com>
2023-02-13 18:44:09 +0000

    chore(vite, PostCSS): add autoprefixer plugin

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import vue from '@vitejs/plugin-vue'
 import path from "path";
+import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    postcss: {
+        plugins: [autoprefixer()]
+    }
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
