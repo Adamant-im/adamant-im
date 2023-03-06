5522dbebee954be981f600033717cda9213484cb
bludnic <victorcpla@gmail.com>
2023-03-06 18:07:48 +0000

    chore: add `prettier`

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
+    '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
