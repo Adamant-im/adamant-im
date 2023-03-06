30cd43b67644e0b6b171e4ffe34ef57bb9dfddc4
bludnic <victorcpla@gmail.com>
2023-03-06 18:05:07 +0000

    chore(package.json): move eslint config to a separate file

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  }
+}
