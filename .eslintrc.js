1073eea24e02b6bedda80b67e8954affe14cf073
bludnic <victorcpla@gmail.com>
2023-03-14 00:02:13 +0000

    fix(.eslintrc.js): add TypeScript plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,8 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
