6f8a3ae7cd6e1e831cb854af68b8b02ebe48d0b0
bludnic <victorcpla@gmail.com>
2023-04-06 15:22:53 +0100

    fix(DelegatesLoader): center loader

--- src/components/DelegatesTable/DelegatesLoader.vue
+++ src/components/DelegatesTable/DelegatesLoader.vue
@@ -2,25 +2,22 @@
   <tr
     class="text-xs-center mt-3 mb-3"
   >
-    <td
-      class="align-center justify-center d-flex"
-      colspan="3"
-    >
-      <v-progress-circular
-        indeterminate
-        color="primary"
-        size="24"
-        class="mr-3"
-      />
-      <div
-        class="a-text-regular"
-        style="line-height:1"
-      >
-        {{ waitingForConfirmation ? $t('votes.waiting_confirmations') : $t('votes.loading_delegates') }}
-      </div>
+    <td colspan="3">
+      <v-row align="center" justify="center">
+        <v-progress-circular
+          indeterminate
+          color="primary"
+          size="24"
+          class="mr-3"
+        />
+        <div
+          class="a-text-regular"
+          style="line-height:1"
+        >
+          {{ waitingForConfirmation ? $t('votes.waiting_confirmations') : $t('votes.loading_delegates') }}
+        </div>
+      </v-row>
     </td>
-    <td class="w-0"></td>
-    <td class="w-0" ></td>
   </tr>
 </template>
 
