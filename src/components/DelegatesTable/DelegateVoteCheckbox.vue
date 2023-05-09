294affa1211e03b0e0f73a0300b2e9dd478a4d2d
David Murasov <pansfloyd@gmail.com>
2023-05-09 13:40:30 +0400

    change how vote icons looks after voting

--- src/components/DelegatesTable/DelegateVoteCheckbox.vue
+++ src/components/DelegatesTable/DelegateVoteCheckbox.vue
@@ -2,14 +2,14 @@
   <v-icon
     v-if="voted"
     :class="classes.icon"
-    icon="mdi-thumb-up"
+    :icon="votes.upVote"
     size="large"
     @click="handleDownVote"
   />
   <v-icon
     v-else
     :class="classes.icon"
-    icon="mdi-thumb-up-outline"
+    :icon="votes.downVote"
     size="large"
     @click="handleUpVote"
   />
@@ -26,33 +26,53 @@ export default {
       required: true
     }
   },
-  setup(props) {
-    const { delegate } = props
+  setup: function (props) {
+    const {delegate} = props
     const store = useStore()
 
     const className = 'delegate-vote-checkbox'
+    const voteName = 'mdi-thumb'
     const classes = {
       root: className,
       icon: `${className}__icon`
     }
+    const votes = {
+      root: voteName,
+      upVote: `${voteName}-up`,
+      downVote: `${voteName}-up-outline`
+    }
 
     const voted = computed(() => delegate._voted)
+    const originalVoted = voted.value;
     const address = computed(() => delegate.address)
 
     const handleUpVote = (event) => {
       event.stopPropagation()
       store.commit('delegates/upVote', address.value)
+      if (originalVoted) {
+        classes.icon = `${className}__icon`
+      } else {
+        classes.icon = `${className}__upvote`
+      }
     }
     const handleDownVote = (event) => {
       event.stopPropagation()
       store.commit('delegates/downVote', address.value)
+
+      if (originalVoted) {
+        classes.icon = `${className}__downvote`
+        votes.downVote = `${voteName}-down-outline`
+      } else {
+        classes.icon = `${className}__icon`
+      }
     }
 
     return {
       voted,
       handleUpVote,
       handleDownVote,
-      classes
+      classes,
+      votes
     }
   }
 }
@@ -67,6 +87,12 @@ export default {
   &__icon {
     font-size: 24px !important;
     height: 24px !important;
+  }
+  &__upvote {
+    color: map-get($adm-colors, 'good');
+  }
+  &__downvote {
+    color: map-get($adm-colors, 'danger');
   }
 }
 
