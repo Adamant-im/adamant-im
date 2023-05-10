28c88d5efd12d42eb75e5e26a6c08be899e23116
David Murasov <pansfloyd@gmail.com>
2023-05-10 15:54:39 +0400

    add v-model to v-checkbox component

--- src/views/Options.vue
+++ src/views/Options.vue
@@ -90,6 +90,7 @@
                 :label="$t('options.stay_logged_in')"
                 color="grey darken-1"
                 :input-value="stayLoggedIn"
+                v-model="stayLoggedIn"
                 readonly
                 density="comfortable"
                 hide-details
