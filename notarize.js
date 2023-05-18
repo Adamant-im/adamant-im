82d56f28fc09db5236a6d23908903bb8fb1ddb3c
bludnic <victorcpla@gmail.com>
2023-05-18 19:45:07 +0100

    feat(Electron Workflow): notarize macOS app

---
+++ notarize.js
@@ -0,0 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { notarize } = require('electron-notarize')
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context
+
+  if (electronPlatformName !== 'darwin') {
+    return
+  }
+
+  const appName = context.packager.appInfo.productFilename
+
+  return await notarize({
+    appBundleId: 'im.adamant.msg',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_ID_PASS
+  })
+}
