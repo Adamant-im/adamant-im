97bccbd493dcd38c88bb7e78b0a48dcbdcb506e2
David Murasov <pansfloyd@gmail.com>
2023-06-22 03:22:45 +0400

    run prettier

--- src/components/QrcodeRenderer.vue
+++ src/components/QrcodeRenderer.vue
@@ -1,9 +1,5 @@
 <template>
-  <img
-    v-if="dataUrl"
-    :src="dataUrl"
-    alt=""
-  >
+  <img v-if="dataUrl" :src="dataUrl" alt="" />
   <div class="spinner-container" v-else>
     <InlineSpinner :size="152" />
   </div>
@@ -33,34 +29,38 @@ export default {
     dataUrl: ''
   }),
   watch: {
-    text () {
+    text() {
       this.render()
     }
   },
-  mounted () {
+  mounted() {
     this.render()
   },
   methods: {
-    render () {
+    render() {
       if (this.text) {
         if (this.logo) {
-          QRCode.toCanvas(this.text, this.opts).then(canvas => {
-            const dHeight = canvas.height / 6
-            const dWidth = canvas.width / 6
-            const dy = canvas.height / 2 - dHeight / 2
-            const dx = canvas.width / 2 - dWidth / 2
-            const ctx = canvas.getContext('2d')
-            const image = new Image(dWidth, dHeight)
-            image.src = this.logo
-            image.onload = () => {
-              ctx.drawImage(image, dx, dy, dWidth, dHeight)
-              this.dataUrl = canvas.toDataURL()
-            }
-          }).catch(error => console.error(error))
+          QRCode.toCanvas(this.text, this.opts)
+            .then((canvas) => {
+              const dHeight = canvas.height / 6
+              const dWidth = canvas.width / 6
+              const dy = canvas.height / 2 - dHeight / 2
+              const dx = canvas.width / 2 - dWidth / 2
+              const ctx = canvas.getContext('2d')
+              const image = new Image(dWidth, dHeight)
+              image.src = this.logo
+              image.onload = () => {
+                ctx.drawImage(image, dx, dy, dWidth, dHeight)
+                this.dataUrl = canvas.toDataURL()
+              }
+            })
+            .catch((error) => console.error(error))
         } else {
-          QRCode.toDataURL(this.text, this.opts).then(dataUrl => {
-            this.dataUrl = dataUrl
-          }).catch(error => console.error(error))
+          QRCode.toDataURL(this.text, this.opts)
+            .then((dataUrl) => {
+              this.dataUrl = dataUrl
+            })
+            .catch((error) => console.error(error))
         }
       }
     }
