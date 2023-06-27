0305013f7cbd7390fac8e7022c826be17cdaa107
martiliones <hi@martilion.es>
2023-06-27 19:25:23 +0600

    fix: use contract IDs from adamant-wallets

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ import dayjs from 'dayjs'
 import WarningOnAddressesDialog from '@/components/WarningOnAddressesDialog.vue'
 import Notifications from '@/lib/notifications'
 import { ThemeName } from './plugins/vuetify'
-import { Cryptos } from '@/lib/constants'
+import { Cryptos, CryptosInfo } from '@/lib/constants'
 
 const loadVuexCoinModules = async () => {
   const ethModule = await import('@/store/modules/eth')
@@ -58,11 +58,11 @@ export default defineComponent({
       this.$store.registerModule('eth', ethModule)
       this.$store.registerModule(
         'bnb',
-        erc20Module(Cryptos.BNB, '0xB8c77482e45F1F44dE1745F52C74426C631bDD52', 18)
+        erc20Module(Cryptos.BNB, CryptosInfo['BNB'].contractId, 18)
       )
       this.$store.registerModule(
         'usds',
-        erc20Module(Cryptos.USDS, '0xa4bdb11dc0a2bec88d24a3aa1e6bb17201112ebe', 6)
+        erc20Module(Cryptos.USDS, CryptosInfo['USDS'].contractId, 6)
       )
     })
   },
