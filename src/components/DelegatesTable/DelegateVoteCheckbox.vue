ebd669f96059343221adefe89b89cdee8fd8cf8f
bludnic <victorcpla@gmail.com>
2023-07-12 00:12:06 +0100

    refactor(DelegatesVoteCheckbox): simplify logic

--- src/components/DelegatesTable/DelegateVoteCheckbox.vue
+++ src/components/DelegatesTable/DelegateVoteCheckbox.vue
@@ -1,23 +1,38 @@
 <template>
   <v-icon
     v-if="voted"
-    :class="classes.icon"
-    :icon="votes.upVote"
+    :class="{
+      [classes.icon]: true,
+      [classes.iconGood]: !originalVoted
+    }"
+    icon="mdi-thumb-up"
     size="large"
     @click="handleDownVote"
   />
+
   <v-icon
     v-else
-    :class="classes.icon"
-    :icon="votes.downVote"
+    :class="{
+      [classes.icon]: true,
+      [classes.iconDanger]: originalVoted
+    }"
+    :icon="originalVoted ? 'mdi-thumb-down-outline' : 'mdi-thumb-up-outline'"
     size="large"
     @click="handleUpVote"
   />
 </template>
 
 <script>
-import { computed, reactive } from 'vue'
+import { computed, toRefs } from 'vue'
 import { useStore } from 'vuex'
+
+const className = 'delegate-vote-checkbox'
+const classes = {
+  root: className,
+  icon: `${className}__icon`,
+  iconGood: `${className}__icon--good`,
+  iconDanger: `${className}__icon--danger`
+}
 
 export default {
   props: {
@@ -26,51 +41,29 @@ export default {
       required: true
     }
   },
-  setup (props) {
-    const { delegate } = props
+  setup(props) {
+    const { delegate } = toRefs(props)
     const store = useStore()
 
-    const className = 'delegate-vote-checkbox'
-    const classes = reactive({
-      root: className,
-      icon: `${className}__icon`
-    })
-    const votes = reactive({
-      upVote: 'mdi-thumb-up',
-      downVote: 'mdi-thumb-up-outline'
-    })
-
-    const voted = computed(() => delegate._voted)
-    const originalVoted = voted.value;
-    const address = computed(() => delegate.address)
+    const voted = computed(() => delegate.value._voted)
+    const originalVoted = computed(() => delegate.value.voted)
+    const address = computed(() => delegate.value.address)
 
     const handleUpVote = (event) => {
       event.stopPropagation()
       store.commit('delegates/upVote', address.value)
-      if (originalVoted) {
-        classes.icon = `${className}__icon`
-      } else {
-        classes.icon = `${className}__icon--good`
-      }
     }
     const handleDownVote = (event) => {
       event.stopPropagation()
       store.commit('delegates/downVote', address.value)
-
-      if (originalVoted) {
-        classes.icon = `${className}__icon--danger`
-        votes.downVote = 'mdi-thumb-down-outline'
-      } else {
-        classes.icon = `${className}__icon`
-      }
     }
 
     return {
+      originalVoted,
       voted,
       handleUpVote,
       handleDownVote,
-      classes,
-      votes
+      classes
     }
   }
 }
@@ -85,9 +78,11 @@ export default {
   &__icon {
     font-size: 24px !important;
     height: 24px !important;
+
     &--good {
       color: map-get($adm-colors, 'good');
     }
+
     &--danger {
       color: map-get($adm-colors, 'danger');
     }
