416060cbdedb45537ec574bed3de58269fe6d68b
bludnic <victorcpla@gmail.com>
2023-07-14 09:04:42 +0100

    fix(useSwipeLeft): fix `onSwipe` trigger condition

--- src/hooks/useSwipeLeft.js
+++ src/hooks/useSwipeLeft.js
@@ -15,6 +15,12 @@ const SWIPE_RATIO_ACTIVATION = 2
  */
 const SWIPE_OFFSET_X_ACTIVATION = 16
 
+/**
+ * Triggers `onSwipe` event when the `offsetX`
+ * reaches `SWIPE_TRIGGER_ACTIVATION` value
+ */
+const SWIPE_TRIGGER_ACTIVATION = 100
+
 export function useSwipeLeft(onSwipe) {
   const swipeStarted = ref(false)
   const elementLeftOffset = ref(0)
@@ -23,18 +29,19 @@ export function useSwipeLeft(onSwipe) {
     const offsetX = e.touchstartX - e.touchmoveX
     const offsetY = e.touchstartY - e.touchmoveY
     const ratio = offsetX / offsetY
+
     const swipeActivated = ratio > SWIPE_RATIO_ACTIVATION && offsetX > SWIPE_OFFSET_X_ACTIVATION
 
     if (swipeActivated) {
       swipeStarted.value = true
     }
 
-    if (offsetX > 100) {
-      onSwipe()
-    }
-
     if (swipeStarted.value) {
       elementLeftOffset.value = -offsetX
+    }
+
+    if (swipeStarted.value && offsetX > SWIPE_TRIGGER_ACTIVATION) {
+      onSwipe()
     }
   }
 
