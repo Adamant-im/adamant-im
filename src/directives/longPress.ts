804029f8d0c9421a6efea1f19aca05b85f1e974e
bludnic <victorcpla@gmail.com>
2023-07-14 09:29:22 +0100

    fix(directives/longPress): enhance directive

--- src/directives/longPress.ts
+++ src/directives/longPress.ts
@@ -9,7 +9,19 @@ export const longPressDirective: Directive = {
       return
     }
 
+    const startX = 0 // mouse x position when timer started
+    const startY = 0 // mouse y position when timer started
+    const maxDiffX = 10 // max number of X pixels the mouse can move during long press before it is canceled
+    const maxDiffY = 10 // max number of Y pixels the mouse can move during long press before it is canceled
+
     let pressTimer: NodeJS.Timeout | null = null
+
+    const clearLongPressTimer = () => {
+      if (pressTimer !== null) {
+        clearTimeout(pressTimer)
+        pressTimer = null
+      }
+    }
 
     const start = (e: TouchEvent) => {
       if (e.type === 'click') {
@@ -21,15 +33,29 @@ export const longPressDirective: Directive = {
       }
     }
 
-    const cancel = () => {
-      if (pressTimer !== null) {
-        clearTimeout(pressTimer)
-        pressTimer = null
+    /**
+     * If the mouse moves n pixels during long-press, cancel the timer
+     */
+    const touchMove = (e: TouchEvent) => {
+      const coordinates = e.touches[0]
+
+      // calculate total number of pixels the pointer has moved
+      const diffX = Math.abs(startX - coordinates.clientX)
+      const diffY = Math.abs(startY - coordinates.clientY)
+
+      // if pointer has moved more than allowed, cancel the long-press timer and therefore the event
+      if (diffX >= maxDiffX || diffY >= maxDiffY) {
+        clearLongPressTimer()
       }
+    }
+
+    const cancel = () => {
+      clearLongPressTimer()
     }
 
     el.addEventListener('touchstart', start)
     el.addEventListener('touchend', cancel)
     el.addEventListener('touchcancel', cancel)
+    el.addEventListener('touchmove', touchMove)
   }
 }
