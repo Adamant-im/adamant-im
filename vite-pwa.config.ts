8856d3128de6c1bf6b1b3b24e9f286f9e6c74a06
bludnic <victorcpla@gmail.com>
2023-07-18 14:32:42 +0100

    build(vite): disable SW in electron app

---
+++ vite-pwa.config.ts
@@ -0,0 +1,27 @@
+import { mergeConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
+import { VitePWA } from 'vite-plugin-pwa'
+
+import viteBaseConfig from './vite-base.config'
+import { manifest } from './vite-config/manifest'
+
+export default mergeConfig(
+  viteBaseConfig,
+  defineConfig({
+    plugins: [
+      VitePWA({
+        registerType: 'autoUpdate',
+        srcDir: 'src',
+        filename: 'service-worker.js',
+        devOptions: {
+          enabled: true
+        },
+        manifest: manifest,
+        manifestFilename: 'manifest.json',
+        workbox: {
+          maximumFileSizeToCacheInBytes: 5000000 // 5 MiB
+        }
+      }),
+    ]
+  })
+)
