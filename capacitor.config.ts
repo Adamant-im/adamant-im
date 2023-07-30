1c5a935e6e81de42fe4f4b12f6ac6dbbef9ad929
bludnic <victorcpla@gmail.com>
2023-07-30 17:23:18 +0100

    feat: add `Capacitor` config

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'im.adamant.adamantmessengerpwa',
+  appName: 'ADAMANT Messenger',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
