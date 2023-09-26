69ef1bb649736e82997477ca25f3e09e3a1b1e24
bludnic <victorcpla@gmail.com>
2023-09-26 20:01:35 +0100

    fix(`chat/helpers/constants`): fix doc block

--- src/lib/chat/helpers/constants.js
+++ src/lib/chat/helpers/constants.js
@@ -3,12 +3,13 @@ import { Cryptos } from '@/lib/constants'
 /**
  * Maps crypto transfer transaction types to the respective cryptos.
  * E.g.:
- ```
- {
-  'eth_transaction': 'ETH',
-  'bnb_transaction': 'BNB'
-}
- ```
+ *
+ * ```
+ * {
+ *   'eth_transaction': 'ETH',
+ *   'bnb_transaction': 'BNB'
+ * }
+ * ```
  */
 export const KnownCryptos = Object.keys(Cryptos).reduce((map, crypto) => {
   if (crypto !== Cryptos.ADM) {
