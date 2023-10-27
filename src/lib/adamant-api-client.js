0b4fe283e15ac7e7e872efb359332a6f8ba85574
bludnic <victorcpla@gmail.com>
2023-10-27 19:44:00 +0100

    refactor(): rewrite healthcheck `AdmNode`

--- src/lib/adamant-api-client.js
+++ src/lib/adamant-api-client.js
@@ -1,6 +1,5 @@
-import axios from 'axios'
+import { AdmNode } from "@/lib/nodes/AdmNode";
 
-import utils from './adamant'
 import config from '../config'
 import semver from 'semver'
 
@@ -16,278 +15,6 @@ const HEIGHT_EPSILON = 10
  * Interval how often to update node statuses
  */
 const REVISE_CONNECTION_TIMEOUT = 5000
-
-/**
- * Protocol on host where app is running, f. e., http: or https:
- */
-const appProtocol = location.protocol
-
-/**
- * @typedef {Object} RequestConfig
- * @property {String} url request relative URL
- * @property {string} method request method (defaults to 'get')
- * @property {any} payload request payload
- */
-
-/**
- * Custom error to indicate that the endpoint is not available
- */
-class NodeOfflineError extends Error {
-  constructor (...args) {
-    super('Node is offline', ...args)
-    this.code = 'NODE_OFFLINE'
-
-    if (Error.captureStackTrace) {
-      Error.captureStackTrace(this, NodeOfflineError)
-    }
-  }
-}
-
-/**
- * Encapsulates an ADAMANT node. Provides methods to send API-requests
- * to the node and verify is status (online/offline, version, ping, etc.)
- */
-class ApiNode {
-  constructor (baseUrl) {
-    /**
-     * Indicates whether node is active (i.e. user allows the application
-     * to interact with this node).
-     * @type {Boolean}
-     */
-    this.active = true
-
-    /**
-     * Indicates whether node is out of sync (i.e. its block height is
-     * either too big or too small compared to the other nodes)
-     * @type {Boolean}
-     */
-    this.outOfSync = false
-
-    this._baseUrl = baseUrl
-    this._protocol = new URL(baseUrl).protocol
-    this._port = new URL(baseUrl).port
-    this._hostname = new URL(baseUrl).hostname
-    this._wsPort = '36668' // default wsPort
-    this._wsProtocol = this._protocol === 'https:' ? 'wss:' : 'ws:'
-    this._wsPortNeeded = this._wsProtocol === 'ws:' && !this._hostname.includes('.onion')
-    this._hasSupportedProtocol = !(this._protocol === 'http:' && appProtocol === 'https:')
-
-    this._online = false
-    this._ping = Infinity
-    this._timeDelta = 0
-    this._version = ''
-    this._height = 0
-    this._socketSupport = false
-
-    this._client = axios.create({
-      baseURL: this._baseUrl
-    })
-  }
-
-  /**
-   * Node base URL
-   * @type {String}
-   */
-  get url () {
-    return this._baseUrl
-  }
-
-  /**
-   * Node port like 36666 for http nodes (default)
-   * @type {String}
-   */
-  get port () {
-    return this._port
-  }
-
-  /**
-   * Node socket port like 36668 (default)
-   * @type {String}
-   */
-  get wsPort () {
-    return this._wsPort
-  }
-
-  /**
-   * Node hostname like bid.adamant.im or 23.226.231.225
-   * @type {String}
-   */
-  get hostname () {
-    return this._hostname
-  }
-
-  /**
-   * Node protocol, like http: or https:
-   * @type {String}
-   */
-  get protocol () {
-    return this._protocol
-  }
-
-  /**
-   * Socket protocol, ws: or wss:
-   * @type {String}
-   */
-  get wsProtocol () {
-    return this._wsProtocol
-  }
-
-  /**
-   * If Socket port like :36668 needed for connection
-   * @type {String}
-   */
-  get wsPortNeeded () {
-    return this._wsPortNeeded
-  }
-
-  /**
-   * Node API version.
-   * @type {String}
-   */
-  get version () {
-    return this._version
-  }
-
-  /**
-   * Indicates whether node is available.
-   * @type {Boolean}
-   */
-  get online () {
-    return this._online
-  }
-
-  /**
-   * Node ping estimation
-   * @type {Number}
-   */
-  get ping () {
-    return this._ping
-  }
-
-  /**
-   * Delta between local time and the node time
-   * @type {Number}
-   */
-  get timeDelta () {
-    return this._timeDelta
-  }
-
-  /**
-   * Current block height
-   * @type {Number}
-   */
-  get height () {
-    return this._height
-  }
-
-  get socketSupport () {
-    return this._socketSupport
-  }
-
-  /**
-   * Performs an API request.
-   *
-   * The `payload` of the `cfg` can be either an object or a function that
-   * accepts `ApiNode` as a first argument and returns an object.
-   * @param {RequestConfig} cfg config
-   * @returns {Promise<any>}
-   */
-  request (cfg) {
-    let { url, method, payload } = cfg
-
-    method = (method || 'get').toLowerCase()
-
-    if (typeof payload === 'function') {
-      payload = payload(this)
-    }
-
-    const config = {
-      url,
-      method,
-      [method === 'get' ? 'params' : 'data']: payload
-    }
-
-    return this._client.request(config).then(
-      response => {
-        const body = response.data
-        // Refresh time delta on each request
-        if (body && isFinite(body.nodeTimestamp)) {
-          this._timeDelta = utils.epochTime() - body.nodeTimestamp
-        }
-
-        return body
-      },
-      error => {
-        // According to https://github.com/axios/axios#handling-errors this means, that request was sent,
-        // but server could not respond.
-        if (!error.response && error.request) {
-          this._online = false
-          throw new NodeOfflineError()
-        }
-        throw error
-      }
-    )
-  }
-
-  /**
-   * Initiates node status update: version, ping, online/offline.
-   * @returns {PromiseLike}
-   */
-  updateStatus () {
-    return this._getNodeStatus()
-      .then(status => {
-        if (status.version) {
-          this._version = status.version
-        }
-        if (status.height) {
-          this._height = status.height
-        }
-        if (status.ping) {
-          this._ping = status.ping
-        }
-        if (status.wsPort) {
-          this._wsPort = status.wsPort
-        }
-        this._online = status.online
-        this._socketSupport = status.socketSupport
-      })
-      .catch(() => {
-        this._online = false
-        this._socketSupport = false
-      })
-  }
-
-  /**
-   * Gets node version, block height and ping.
-   * @returns {Promise<{version: string, height: number, ping: number}>}
-   */
-  _getNodeStatus () {
-    if (!this._hasSupportedProtocol) {
-      return new Promise((resolve) => {
-        resolve({
-          online: false,
-          socketSupport: false
-        })
-      })
-    } else {
-      const time = Date.now()
-      return this.request({ url: '/api/node/status' })
-        .then(res => {
-          if (res.success) {
-            return {
-              online: true,
-              version: res.version.version,
-              height: Number(res.network.height),
-              ping: Date.now() - time,
-              socketSupport: res.wsClient && res.wsClient.enabled,
-              wsPort: res.wsClient ? res.wsClient.port : false
-            }
-          }
-          throw new Error('Request to /api/node/status was unsuccessful')
-        })
-    }
-  }
-}
 
 /**
  * Provides methods for calling the ADAMANT API.
@@ -306,7 +33,7 @@ class ApiClient {
      * List of the available nodes
      * @type {Array<ApiNode>}
      */
-    this._nodes = endpoints.map(x => new ApiNode(x))
+    this._nodes = endpoints.map(x => new AdmNode(x))
 
     /**
      * Minimum API version a node is required to have
@@ -329,17 +56,17 @@ class ApiClient {
       url: node.url,
       port: node.port,
       hostname: node.hostname,
-      protocol: node._protocol,
-      wsProtocol: node._wsProtocol,
-      wsPort: node._wsPort,
-      wsPortNeeded: node._wsPortNeeded,
+      protocol: node.protocol,
+      wsProtocol: node.wsProtocol,
+      wsPort: node.wsPort,
+      wsPortNeeded: node.wsPortNeeded,
       online: node.online,
       ping: node.ping,
       version: node.version,
       active: node.active,
       outOfSync: node.outOfSync,
       hasMinNodeVersion: node.version >= this._minNodeVersion,
-      hasSupportedProtocol: node._hasSupportedProtocol,
+      hasSupportedProtocol: node.hasSupportedProtocol,
       socketSupport: node.socketSupport
     })
 
---
+++ src/lib/nodes/AdmNode.ts
@@ -0,0 +1,233 @@
+import utils from '@/lib/adamant'
+import { GetNodeStatusResponseDto } from '@/lib/schema/client'
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios'
+
+/**
+ * Protocol on host where app is running, f. e., http: or https:
+ */
+const appProtocol = location.protocol
+
+/**
+ * Custom error to indicate that the endpoint is not available
+ */
+class NodeOfflineError extends Error {
+  code = 'NODE_OFFLINE'
+
+  constructor() {
+    super('Node is offline')
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, NodeOfflineError)
+    }
+  }
+}
+
+type NodeStatus = {
+  online: boolean
+  socketSupport: boolean
+  version?: string
+  height?: number
+  ping?: number
+  wsPort?: string
+}
+
+type Payload =
+  | Record<string, any>
+  | {
+      (ctx: AdmNode): Record<string, any>
+    }
+type RequestConfig<P extends Payload> = {
+  url: string
+  method?: string
+  payload?: P
+}
+
+/**
+ * Encapsulates a node. Provides methods to send API-requests
+ * to the node and verify is status (online/offline, version, ping, etc.)
+ */
+export class AdmNode {
+  /**
+   * Indicates whether node is active (i.e. user allows the application
+   * to interact with this node).
+   */
+  active = true
+
+  /**
+   * Indicates whether node is out of sync (i.e. its block height is
+   * either too big or too small compared to the other nodes)
+   */
+  outOfSync = false
+
+  /**
+   * Default `wsPort`. Will be updated after `GET /api/node/status`
+   */
+  wsPort = '36668'
+
+  /**
+   * Node base URL
+   */
+  baseUrl: string
+  /**
+   * Node protocol, like http: or https:
+   */
+  protocol: string
+  /**
+   * Node port like 36666 for http nodes (default)
+   */
+  port: string
+  /**
+   * Node hostname like bid.adamant.im or 23.226.231.225
+   */
+  hostname: string
+  /**
+   * WebSocket protocol
+   */
+  wsProtocol: 'ws:' | 'wss:'
+  /**
+   * If Socket port like :36668 needed for connection
+   */
+  wsPortNeeded: boolean
+  hasSupportedProtocol: boolean
+
+  // Healthcheck related params
+  /**
+   * Indicates whether node is available.
+   */
+  online = false
+  /**
+   * Node ping estimation
+   */
+  ping = Infinity
+  /**
+   * Delta between local time and the node time
+   */
+  timeDelta = 0
+  /**
+   * Node API version.
+   */
+  version = ''
+  /**
+   * Current block height
+   */
+  height = 0
+  /**
+   * Will be updated after `GET /api/node/status`
+   */
+  socketSupport = false
+
+  client: AxiosInstance
+
+  constructor(baseUrl: string) {
+    this.baseUrl = baseUrl
+    this.protocol = new URL(baseUrl).protocol
+    this.port = new URL(baseUrl).port
+    this.hostname = new URL(baseUrl).hostname
+    this.wsPort = '36668' // default wsPort
+    this.wsProtocol = this.protocol === 'https:' ? 'wss:' : 'ws:'
+    this.wsPortNeeded = this.wsProtocol === 'ws:' && !this.hostname.includes('.onion')
+    this.hasSupportedProtocol = !(this.protocol === 'http:' && appProtocol === 'https:')
+
+    this.client = axios.create({
+      baseURL: this.baseUrl
+    })
+  }
+
+  get url() {
+    return this.baseUrl
+  }
+
+  /**
+   * Performs an API request.
+   *
+   * The `payload` of the `cfg` can be either an object or a function that
+   * accepts `ApiNode` as a first argument and returns an object.
+   */
+  request<P extends Payload = Payload>(cfg: RequestConfig<P>) {
+    const { url, method = 'get', payload } = cfg
+
+    const config: AxiosRequestConfig = {
+      url,
+      method: method.toLowerCase(),
+      [method === 'get' ? 'params' : 'data']:
+        typeof payload === 'function' ? payload(this) : payload
+    }
+
+    return this.client.request(config).then(
+      (response) => {
+        const body = response.data
+        // Refresh time delta on each request
+        if (body && isFinite(body.nodeTimestamp)) {
+          this.timeDelta = utils.epochTime() - body.nodeTimestamp
+        }
+
+        return body
+      },
+      (error) => {
+        // According to https://github.com/axios/axios#handling-errors this means, that request was sent,
+        // but server could not respond.
+        if (!error.response && error.request) {
+          this.online = false
+          throw new NodeOfflineError()
+        }
+        throw error
+      }
+    )
+  }
+
+  /**
+   * Initiates node status update: version, ping, online/offline.
+   * @returns {PromiseLike}
+   */
+  async updateStatus() {
+    try {
+      const status = await this.getNodeStatus()
+
+      if (status.version) {
+        this.version = status.version
+      }
+      if (status.height) {
+        this.height = status.height
+      }
+      if (status.ping) {
+        this.ping = status.ping
+      }
+      if (status.wsPort) {
+        this.wsPort = status.wsPort
+      }
+      this.online = status.online
+      this.socketSupport = status.socketSupport
+    } catch (err) {
+      this.online = false
+      this.socketSupport = false
+    }
+  }
+
+  /**
+   * Gets node version, block height and ping.
+   * @returns {Promise<{version: string, height: number, ping: number}>}
+   */
+  private async getNodeStatus(): Promise<NodeStatus> {
+    if (!this.hasSupportedProtocol) {
+      return Promise.reject({
+        online: false,
+        socketSupport: false
+      })
+    }
+
+    const time = Date.now()
+    const response: GetNodeStatusResponseDto = await this.request({ url: '/api/node/status' })
+    if (response.success) {
+      return {
+        online: true,
+        version: response.version.version,
+        height: Number(response.network.height),
+        ping: Date.now() - time,
+        socketSupport: response.wsClient ? response.wsClient.enabled : false,
+        wsPort: response.wsClient ? String(response.wsClient.port) : undefined
+      }
+    }
+
+    throw new Error('Request to /api/node/status was unsuccessful')
+  }
+}
