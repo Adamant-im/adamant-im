648912cbcd7a753449cbe69c4bb1a82848c70359
bludnic <victorcpla@gmail.com>
2023-11-02 17:05:12 +0000

    refactor(nodes, Vuex): dry

--- src/store/modules/nodes/nodes-plugin.js
+++ src/store/modules/nodes/nodes-plugin.js
@@ -1,45 +1,22 @@
-import { adm } from '@/lib/nodes/adm'
-import { eth } from '@/lib/nodes/eth'
-import { btc } from '@/lib/nodes/btc'
-import { doge } from '@/lib/nodes/doge'
-import { dash } from '@/lib/nodes/dash'
-import { lsk } from '@/lib/nodes/lsk'
+import { nodes } from '@/lib/nodes'
 
 export default (store) => {
   // initial nodes state
-  adm.getNodes().forEach((status) => store.commit('nodes/status', { status, nodeType: 'adm' }))
-  eth.getNodes().forEach((status) => store.commit('nodes/status', { status, nodeType: 'eth' }))
-  btc.getNodes().forEach((status) => store.commit('nodes/status', { status, nodeType: 'btc' }))
-  doge.getNodes().forEach((status) => store.commit('nodes/status', { status, nodeType: 'doge' }))
-  dash.getNodes().forEach((status) => store.commit('nodes/status', { status, nodeType: 'dash' }))
-  lsk.getNodes().forEach((status) => store.commit('nodes/status', { status, nodeType: 'lsk' }))
+  for (const [nodeType, client] of Object.entries(nodes)) {
+    client.getNodes().forEach((status) => store.commit('nodes/status', { status, nodeType }))
+
+    client.onStatusUpdate((status) => {
+      store.commit('nodes/status', { status, nodeType })
+    })
+  }
 
   store.subscribe((mutation) => {
     if (mutation.type === 'nodes/useFastest') {
-      adm.useFastest = !!mutation.payload
+      nodes.adm.useFastest = !!mutation.payload
     }
 
     if (mutation.type === 'nodes/toggle') {
-      adm.toggleNode(mutation.payload.url, mutation.payload.active)
+      nodes.adm.toggleNode(mutation.payload.url, mutation.payload.active)
     }
-  })
-
-  adm.onStatusUpdate((status) => {
-    store.commit('nodes/status', { status, nodeType: 'adm' })
-  })
-  eth.onStatusUpdate((status) => {
-    store.commit('nodes/status', { status, nodeType: 'eth' })
-  })
-  btc.onStatusUpdate((status) => {
-    store.commit('nodes/status', { status, nodeType: 'btc' })
-  })
-  doge.onStatusUpdate((status) => {
-    store.commit('nodes/status', { status, nodeType: 'doge' })
-  })
-  dash.onStatusUpdate((status) => {
-    store.commit('nodes/status', { status, nodeType: 'dash' })
-  })
-  lsk.onStatusUpdate((status) => {
-    store.commit('nodes/status', { status, nodeType: 'lsk' })
   })
 }
