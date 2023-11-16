f82b0a92ab9c9a6565a52bee25a2100809315692
bludnic <victorcpla@gmail.com>
2023-11-16 20:35:28 +0000

    feat(Transaction): show inconsistent status if it's a duplicate

--- src/store/modules/chat/index.js
+++ src/store/modules/chat/index.js
@@ -385,19 +385,6 @@ const mutations = {
       return
     }
 
-    // Shouldn't duplicate third-party crypto transactions
-    if (
-      message.type &&
-      message.type !== 'message' &&
-      message.type !== 'reaction' &&
-      message.type !== Cryptos.ADM
-    ) {
-      const localTransaction = chat.messages.find(
-        (localTransaction) => localTransaction.hash === message.hash
-      )
-      if (localTransaction) return
-    }
-
     // use unshift when loading chat history
     if (unshift) {
       chat.messages.unshift(message)
