88ae539c093159e3a37edcc9c458bb10f8bcd813
Mikhail <realgoodprogrammer@gmail.com>
2023-12-08 15:37:11 +0300

    feat: Choose visible wallets. Draggable

--- src/views/Wallets.vue
+++ src/views/Wallets.vue
@@ -4,28 +4,123 @@
 
     <v-container fluid class="px-0 container--with-app-toolbar">
       <v-row justify="center" no-gutters>
-        <container padding> </container>
+        <container padding>
+          <v-list lines="two">
+            <draggable
+              class="list-group"
+              :component-data="{
+                tag: 'ul',
+                type: 'transition-group',
+                name: !isDragging ? 'flip-list' : null
+              }"
+              v-model="wallets"
+              v-bind="dragOptions"
+              @start="isDragging = true"
+              @end="isDragging = false"
+              item-key="cryptoName"
+            >
+              <template #item="{ element }">
+                <v-list-item :title="element.cryptoName">
+                  <v-list-item-subtitle>
+                    <p>
+                      {{ element.type }} <b>{{ element.symbol }}</b>
+                    </p>
+                  </v-list-item-subtitle>
+                  <template v-slot:prepend>
+                    <v-avatar>
+                      <crypto-icon
+                        :class="classes.cryptoIcon"
+                        :crypto="element.cryptoCurrency"
+                        size="medium"
+                      />
+                    </v-avatar>
+                  </template>
+
+                  <template v-slot:append>
+                    <v-checkbox hide-details></v-checkbox>
+                    <v-btn
+                      color="grey-lighten-1"
+                      class="handle"
+                      icon="mdi-menu"
+                      variant="text"
+                    ></v-btn>
+                  </template>
+                </v-list-item>
+              </template>
+            </draggable>
+          </v-list>
+        </container>
       </v-row>
     </v-container>
   </div>
 </template>
 
 <script lang="ts">
+import draggable from 'vuedraggable'
 import AppToolbarCentered from '@/components/AppToolbarCentered.vue'
-import { defineComponent } from 'vue'
+import { computed, defineComponent, reactive, ref } from 'vue'
+import { CryptosInfo, CryptosOrder, isErc20 } from '@/lib/constants'
+import CryptoIcon from '@/components/icons/CryptoIcon.vue'
 
 const className = 'wallets-view'
 const classes = {
-  root: className
+  root: className,
+  cryptoIcon: `${className}__crypto-icon`
+}
+
+// TODO
+type Wallet = {
+  address?: string
+  balance?: number
+  cryptoCurrency: string
+  cryptoName: string
+  erc20: boolean
+  hasBalanceLoaded?: boolean
+  isBalanceLoading?: boolean
+  rate?: number
+  symbol: string
+  type: string
 }
 
 export default defineComponent({
   components: {
-    AppToolbarCentered
+    AppToolbarCentered,
+    CryptoIcon,
+    draggable
+  },
+  beforeMount() {
+    this.wallets = CryptosOrder.map((crypto) => {
+      const erc20 = isErc20(crypto.toUpperCase())
+      const cryptoName = CryptosInfo[crypto].nameShort || CryptosInfo[crypto].name
+      const symbol = CryptosInfo[crypto].symbol
+      const type = CryptosInfo[crypto].type ?? 'Blockchain'
+
+      return {
+        cryptoName,
+        erc20,
+        cryptoCurrency: crypto,
+        symbol,
+        type
+      }
+    })
   },
   setup() {
+    const isDragging = ref(false)
+    const wallets = reactive<Array<Wallet>>([])
+    const dragOptions = computed(() => {
+      return {
+        animation: 200,
+        disabled: false,
+        ghostClass: 'ghost',
+        group: 'description'
+      }
+    })
+
     return {
-      classes
+      classes,
+      dragOptions,
+      isDragging,
+      wallets
     }
   }
 })
@@ -51,6 +146,10 @@ export default defineComponent({
 
   :deep(.v-checkbox) {
     margin-left: -8px;
+  }
+
+  :deep(.sortable-chosen) {
+    background: #2e7eed; //TODO: temp style
   }
 }
 
