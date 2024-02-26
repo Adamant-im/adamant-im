d321081990071ebd1d99a93631a14603749286c6
bludnic <victorcpla@gmail.com>
2024-02-26 16:13:41 +0000

    refactor(lib/display-mobile): type is inherited

--- src/lib/display-mobile.ts
+++ src/lib/display-mobile.ts
@@ -2,7 +2,7 @@ import { detect } from 'detect-browser'
 
 export function isMobile(): boolean {
   const browser = detect()
-  const isMobileDevice: boolean =
+  const isMobileDevice =
     browser && browser.os ? ['android', 'ios'].includes(browser.os.toLowerCase()) : false
   return isMobileDevice || window.innerWidth < 450
 }
