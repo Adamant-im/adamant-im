a2f5baca798743ea3a7aaa486ec411b7b48a8cb8
bludnic <victorcpla@gmail.com>
2024-05-08 00:48:23 +0100

    fix(nodes, storage): fix undefined error

--- src/lib/nodes/storage.ts
+++ src/lib/nodes/storage.ts
@@ -48,10 +48,17 @@ export const nodesStorage = {
   getUseFastest(node: NodeType) {
     const options = optionsStorage.getItem()
 
-    return options[node].useFastest
+    return options[node]?.useFastest
   },
   setUseFastest(value: boolean, node: NodeType) {
     const options = optionsStorage.getItem()
+
+    if (!options[node]) {
+      options[node] = {
+        useFastest: value
+      }
+    }
+
     options[node].useFastest = value
 
     optionsStorage.setItem(options)
