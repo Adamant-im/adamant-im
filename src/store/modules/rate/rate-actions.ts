34f1ca60466987151a996b61323ad1f400ab0343
Mikhail <realgoodprogrammer@gmail.com>
2024-07-13 00:09:43 +0300

    fix(rate-actions): removed Promise wrapper

--- src/store/modules/rate/rate-actions.ts
+++ src/store/modules/rate/rate-actions.ts
@@ -8,41 +8,23 @@ export let interval: NodeJS.Timeout
 const UPDATE_RATES_INTERVAL = 90000
 
 export const actions: ActionTree<RateState, RootState> = {
-  getAllRates({ commit }) {
-    return new Promise((resolve, reject) => {
-      rateInfoClient
-        .getAllRates()
-        .then(({ result, success }) => {
-          if (success) {
-            commit('setRates', result)
-          }
-          commit('loadRates')
-          resolve(result)
-        })
-        .catch((err) => {
-          reject(err)
-        })
-    })
+  async getAllRates({ commit }) {
+    const { result, success } = await rateInfoClient.getAllRates()
+    if (success) {
+      commit('setRates', result)
+    }
+    commit('loadRates')
   },
-  getHistoryRates({ state, commit }, { timestamp }) {
+  async getHistoryRates({ state, commit }, { timestamp }) {
     if (!timestamp) return
     if (state.historyRates[timestamp] !== undefined) {
       return state.historyRates[timestamp]
     } else {
-      return new Promise((resolve, reject) => {
-        rateInfoClient
-          .getHistory(timestamp)
-          .then(({ result, success }) => {
-            if (success) {
-              const rates = result[0]?.tickers ?? null
-              commit('setHistoryRates', { name: timestamp, value: rates })
-            }
-            resolve(result)
-          })
-          .catch((err) => {
-            reject(err)
-          })
-      })
+      const { result, success } = await rateInfoClient.getHistory(timestamp)
+      if (success) {
+        const rates = result[0]?.tickers ?? null
+        commit('setHistoryRates', { name: timestamp, value: rates })
+      }
     }
   },
   startInterval: {
