6ff54a7f19db8a2d6183b02a699ddf7861d18ec7
bludnic <victorcpla@gmail.com>
2024-07-15 21:22:43 +0100

    refactor(useKlyTransferQuery): simplify TS type

--- src/hooks/queries/useTransferQuery/useKlyTransferQuery.ts
+++ src/hooks/queries/useTransferQuery/useKlyTransferQuery.ts
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/vue-query'
 import { computed, MaybeRef, unref } from 'vue'
 import { useStore } from 'vuex'
-
+import { KlyTransaction } from '@/lib/nodes/types/transaction'
 import { refetchIntervalFactory, retryDelayFactory, retryFactory } from './utils'
 import { Cryptos } from '@/lib/constants'
 import { klyIndexer } from '@/lib/nodes'
@@ -13,7 +13,7 @@ export function useKlyTransferQuery(transactionId: MaybeRef<string>) {
   return useQuery({
     queryKey: ['transaction', Cryptos.KLY, transactionId],
     queryFn: () => klyIndexer.getTransaction(unref(transactionId), unref(klyAddress)),
-    initialData: {} as Awaited<ReturnType<typeof klyIndexer.getTransaction>>,
+    initialData: {} as KlyTransaction,
     retry: retryFactory(Cryptos.BTC, unref(transactionId)),
     retryDelay: retryDelayFactory(Cryptos.BTC, unref(transactionId)),
     refetchInterval: refetchIntervalFactory(Cryptos.BTC)
