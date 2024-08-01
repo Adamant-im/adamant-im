6075e61499865e58c0f66fea4a2caa0edbd7cf81
bludnic <victorcpla@gmail.com>
2024-08-01 06:02:08 +0100

    chore(vite, plugin): convert to TS

--- vite-config/plugins/excludeScreenshotsPlugin.ts
+++ vite-config/plugins/excludeScreenshotsPlugin.ts
@@ -1,19 +1,23 @@
 import fs from 'node:fs'
-import path from 'path'
+import path from 'node:path'
+import { Plugin } from 'vite'
 
 /**
  * Exclude screenshots from Electron bundle
  */
-export function excludeScreenshotsPlugin () {
+export function excludeScreenshotsPlugin(): Plugin {
   return {
     name: 'exclude-screenshots-plugin',
-    resolveId (source) {
+    resolveId(source) {
       return source === 'virtual-module' ? source : null
     },
-    writeBundle (outputOptions, inputOptions) {
-      const outDir = outputOptions.dir;
-      const screenshotsDir = path.resolve(outDir, 'screenshots');
-      fs.rm(screenshotsDir, { recursive: true }, () => console.log(`Deleted ${screenshotsDir}`))
+    writeBundle(outputOptions) {
+      const outDir = outputOptions.dir as string
+      const screenshotsDir = path.resolve(outDir, 'screenshots')
+
+      fs.rm(screenshotsDir, { recursive: true }, () =>
+        console.log(`Deleted screenshots from ${screenshotsDir}`)
+      )
     }
   }
 }
