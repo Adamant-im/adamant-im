c5abb4cc81c394c395b8488ac78737f5134fe14e
bludnic <victorcpla@gmail.com>
2024-09-28 13:39:06 +0100

    chore(ServiceWorker): remove hash from MDI font files

--- vite-base.config.ts
+++ vite-base.config.ts
@@ -69,7 +69,16 @@ export default defineConfig({
       include: []
     },
     rollupOptions: {
-      external: [...excludeBip39Wordlists()]
+      external: [...excludeBip39Wordlists()],
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name.startsWith('materialdesignicons-webfont')) {
+            return 'assets/[name][extname]'
+          }
+
+          return 'assets/[name]-[hash][extname]'
+        }
+      }
     }
   }
 })
