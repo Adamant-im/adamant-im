cc5537c79769db2f5c375ff303914de1b528f66c
bludnic <victorcpla@gmail.com>
2024-10-10 22:16:38 +0100

    refactor(AChatImageModalItem): fix types

--- src/components/AChat/AChatAttachment/AChatImageModalItem.vue
+++ src/components/AChat/AChatAttachment/AChatImageModalItem.vue
@@ -6,12 +6,8 @@
 import { defineComponent, PropType, ref, onMounted, computed } from 'vue'
 import { useStore } from 'vuex'
 
-import { LocalFile, NormalizedChatMessageTransaction } from '@/lib/chat/helpers'
+import { NormalizedChatMessageTransaction } from '@/lib/chat/helpers'
 import { FileAsset } from '@/lib/adamant-api/asset'
-
-function isLocalFile(file: FileAsset | LocalFile): file is LocalFile {
-  return 'file' in file && file.file?.file instanceof File
-}
 
 const className = 'a-chat-image-modal-item'
 const classes = {
@@ -21,7 +17,7 @@ const classes = {
 export default defineComponent({
   props: {
     file: {
-      type: Object as PropType<FileAsset | LocalFile>,
+      type: Object as PropType<FileAsset>,
       required: true
     },
     transaction: {
@@ -33,42 +29,22 @@ export default defineComponent({
     const store = useStore()
     const imageUrl = ref('')
 
-    const width = computed(() => {
-      if (isLocalFile(props.file)) {
-        return props.file.file.width
-      } else {
-        return props.file.resolution?.[0]
-      }
-    })
-    const height = computed(() => {
-      if (isLocalFile(props.file)) {
-        return props.file.file.height
-      } else {
-        return props.file.resolution?.[1]
-      }
-    })
+    const width = computed(() => props.file.resolution?.[0])
+    const height = computed(() => props.file.resolution?.[1])
+    const publicKey = computed(() =>
+      props.transaction.senderId === store.state.address
+        ? props.transaction.recipientPublicKey
+        : props.transaction.senderPublicKey
+    )
 
     const getFileFromStorage = async () => {
-      const myAddress = store.state.address
+      const { id, nonce } = props.file
 
-      const cid = props.file?.id
-      const fileName = props.file?.name
-      const fileType = props.file?.type
-      const nonce = props.file?.nonce
-
-      const publicKey =
-        props.transaction.senderId === myAddress
-          ? props.transaction.recipientPublicKey
-          : props.transaction.senderPublicKey
       imageUrl.value = await store.dispatch('attachment/getAttachmentUrl', {
-        cid,
-        publicKey,
+        cid: id,
+        publicKey: publicKey.value,
         nonce
       })
-      if (!!fileName && !!fileType) {
-        // TODO: resolve MIME-type
-        // downloadFile(data, fileName, '')
-      }
     }
 
     onMounted(() => {
