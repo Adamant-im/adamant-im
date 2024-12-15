47b6f0aa0454eca6231af435a4ebf08e2a322fc6
Ivanov N <nik@tmails.net>
2024-12-15 22:03:01 +0300

    checkUrl https

--- src/store/plugins/navigatorOnline.js
+++ src/store/plugins/navigatorOnline.js
@@ -1,7 +1,7 @@
 import { i18n } from '@/i18n'
 import axios from 'axios'
 
-const checkUrl = 'http://ipv4.icanhazip.com'
+const checkUrl = 'https://ipv4.icanhazip.com'
 
 export default (store) => {
   // window.addEventListener('online', handleEvent)
@@ -14,33 +14,31 @@ export default (store) => {
   //     store.commit('setIsOnline', false)
   //   }
 
-    setInterval(async () => {
-      const res = Boolean(await inetConnectionCheck())
-      const storeVal = store.getters['isOnline']
-      if (storeVal !== res) {
-        store.commit('setIsOnline', res)
-        const snackMsg = res ? 'online' : 'offline'
-        store.dispatch('snackbar/show', {
-          message: i18n.global.t(`connection.${snackMsg}`),
-          timeout: 3000
-        })
-      }
-    }, 1000)
-  }
-
-  const inetConnectionCheck = async () => {
-    try {
-      const res = await axios.get(`${checkUrl}`,
-        {
-          headers: {
-            'Cache-Control': 'no-cache',
-            'Pragma': 'no-cache',
-            'Expires': '0',
-          },
-        }
-      )
-      return res.status >=200 && res.status < 300
-    } catch(err) {
-      if (err.code === 'ERR_NETWORK') return false
+  setInterval(async () => {
+    const res = Boolean(await inetConnectionCheck())
+    const storeVal = store.getters['isOnline']
+    if (storeVal !== res) {
+      store.commit('setIsOnline', res)
+      const snackMsg = res ? 'online' : 'offline'
+      store.dispatch('snackbar/show', {
+        message: i18n.global.t(`connection.${snackMsg}`),
+        timeout: 3000
+      })
     }
+  }, 1000)
+}
+
+const inetConnectionCheck = async () => {
+  try {
+    const res = await axios.get(`${checkUrl}`, {
+      headers: {
+        'Cache-Control': 'no-cache',
+        Pragma: 'no-cache',
+        Expires: '0'
+      }
+    })
+    return res.status >= 200 && res.status < 300
+  } catch (err) {
+    if (err.code === 'ERR_NETWORK') return false
   }
+}
