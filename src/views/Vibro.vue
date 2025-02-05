af4b1fe0cc69b98f52a95e19af16c35ece52c9d8
omen <s.frontend.dev@gmail.com>
2025-02-05 21:18:24 +0700

    feat: change icons for vibro page

--- src/views/Vibro.vue
+++ src/views/Vibro.vue
@@ -2,43 +2,43 @@
   <div>
     <input v-model="vibro" class="input" placeholder="Example 30,20,30" />
     <v-btn @click="vibration" class="btnPlay"> play</v-btn>
-    <v-btn @click="veryShort" class="btn" append-icon="mdi-play"
+    <v-btn @click="veryShort" class="btn" :append-icon="mdiPlay"
       >Very short
       <template v-slot:append>
         <v-icon color="warning"></v-icon>
       </template>
     </v-btn>
-    <v-btn @click="short" class="btn" append-icon="mdi-play"
+    <v-btn @click="short" class="btn" :append-icon="mdiPlay"
       >Short
       <template v-slot:append>
         <v-icon color="warning"></v-icon>
       </template>
     </v-btn>
-    <v-btn @click="medium" class="btn" append-icon="mdi-play"
+    <v-btn @click="medium" class="btn" :append-icon="mdiPlay"
       >Medium
       <template v-slot:append>
         <v-icon color="warning"></v-icon>
       </template>
     </v-btn>
-    <v-btn @click="long" class="btn" append-icon="mdi-play"
+    <v-btn @click="long" class="btn" :append-icon="mdiPlay"
       >Long
       <template v-slot:append>
         <v-icon color="warning"></v-icon>
       </template>
     </v-btn>
-    <v-btn @click="doubleVeryShort" class="btn" append-icon="mdi-play"
+    <v-btn @click="doubleVeryShort" class="btn" :append-icon="mdiPlay"
       >Double-v-Short
       <template v-slot:append>
         <v-icon color="warning"></v-icon>
       </template>
     </v-btn>
-    <v-btn @click="tripleVeryShort" class="btn" append-icon="mdi-play"
+    <v-btn @click="tripleVeryShort" class="btn" :append-icon="mdiPlay"
       >Triple-v-Short
       <template v-slot:append>
         <v-icon color="warning"></v-icon>
       </template>
     </v-btn>
-    <v-btn @click="doubleVeryShort" class="btn" append-icon="mdi-play"
+    <v-btn @click="doubleVeryShort" class="btn" :append-icon="mdiPlay"
       >Double-Short
       <template v-slot:append>
         <v-icon color="warning"></v-icon>
@@ -48,10 +48,18 @@
 </template>
 <script>
 import { vibrate } from '@/lib/vibrate'
+import { mdiPlay } from '@mdi/js'
+
 export default {
   data: () => ({
     vibro: undefined
   }),
+
+  setup() {
+    return {
+      mdiPlay
+    }
+  },
 
   methods: {
     vibration() {
