cfb52cad76e7bdeb21789b0dd8f6cd932fd7bca4
omen <s.frontend.dev@gmail.com>
2025-02-06 21:58:33 +0700

    fix: add normalizeLineSeparator helper

--- src/lib/markdown.js
+++ src/lib/markdown.js
@@ -33,6 +33,22 @@ renderer.heading = function ({ text }) {
 
 marked.use({ renderer })
 
+
+/**
+ * Normalizes line separator characters in the given text by replacing U+2028 (LINE SEPARATOR) characters with the specified line separator character.
+ *
+ * The U+2028 character (LINE SEPARATOR) is sometimes used as a line break, but it is treated as a space in some web environments,
+ * causing unexpected rendering issues. To avoid this, it's recommended to replace it with a standard line break character such as `\n`.
+ *
+ * @param {string} text - The text in which line separator characters need to be replaced.
+ * @param {string} [replaceValue="\n"] - The value that will replace the U+2028 character. Defaults to the newline character (`\n`).
+ *
+ * @returns {string} - The text with U+2028 characters replaced by `replaceValue`.
+ *
+ */
+const normalizeLineSeparator = (text, replaceValue = "\n") => {
+  return text.replace(/\u2028/g, replaceValue);
+}
 /**
  * Sanitizes text to show HTML
  * @param {string} text text to sanitize
@@ -48,7 +64,7 @@ export function sanitizeHTML(text = '') {
  * @returns {string} resulting sanitized HTML
  */
 export function renderMarkdown(text = '') {
-  return marked.parse(sanitizeHTML(text.replace(/\u2028/g, "\n")))
+  return marked.parse(sanitizeHTML(normalizeLineSeparator(text)))
 }
 
 /**
@@ -71,6 +87,8 @@ export function formatMessage(text = '') {
   node.innerHTML = marked.parse(sanitizeHTML(textWithSymbol))
 
   const textWithoutHtml = node.textContent || node.innerText || ''
-  const styledText = textWithoutHtml.replace(/↵/g, '<span class="arrow-return">↵</span>').replace(/\u2028/g, '<span class="arrow-return">↵</span>')
-  return styledText
+
+  const replaceValue = '<span class="arrow-return">↵</span>';
+
+  return normalizeLineSeparator(textWithoutHtml.replace(/↵/g, replaceValue), replaceValue)
 }
