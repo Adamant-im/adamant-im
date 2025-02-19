30fd915676c5a3ddad59305fa3b21a6f92df011a
omen <s.frontend.dev@gmail.com>
2025-02-19 17:59:32 +0700

    feat: Erc20Transaction script setup

--- src/components/transactions/Erc20Transaction.vue
+++ src/components/transactions/Erc20Transaction.vue
@@ -17,8 +17,8 @@
   />
 </template>
 
-<script lang="ts">
-import { computed, defineComponent, PropType } from 'vue'
+<script lang="ts" setup>
+import { computed, PropType } from 'vue'
 import { useStore } from 'vuex'
 import TransactionTemplate from './TransactionTemplate.vue'
 import { getExplorerTxUrl } from '@/config/utils'
@@ -33,89 +33,66 @@ import { useErc20TransactionQuery } from '@/hooks/queries/transaction'
 import { useClearPendingTransaction } from './hooks/useClearPendingTransaction'
 import { getPartnerAddress } from './utils/getPartnerAddress'
 
-export default defineComponent({
-  name: 'Erc20Transaction',
-  components: {
-    TransactionTemplate
+const props = defineProps({
+  crypto: {
+    required: true,
+    type: String as PropType<CryptoSymbol>
   },
-  props: {
-    crypto: {
-      required: true,
-      type: String as PropType<CryptoSymbol>
-    },
-    id: {
-      required: true,
-      type: String
-    }
-  },
-  setup(props) {
-    const store = useStore()
-
-    const cryptoAddress = computed(() => store.state.eth.address)
-
-    const {
-      status: queryStatus,
-      isFetching,
-      data: transaction,
-      refetch
-    } = useErc20TransactionQuery(props.crypto)(props.id)
-    const inconsistentStatus = useInconsistentStatus(transaction, props.crypto)
-    const transactionStatus = useTransactionStatus(isFetching, queryStatus, inconsistentStatus)
-    useClearPendingTransaction(props.crypto, transaction)
-
-    const admTx = useFindAdmTransaction(props.id)
-    const senderAdmAddress = computed(() => admTx.value?.senderId || '')
-    const recipientAdmAddress = computed(() => admTx.value?.recipientId || '')
-    const partnerAdmAddress = computed(() =>
-      admTx.value
-        ? getPartnerAddress(admTx.value?.senderId, admTx.value?.recipientId, cryptoAddress.value)
-        : ''
-    )
-
-    const senderFormatted = useCryptoAddressPretty(
-      transaction,
-      cryptoAddress,
-      senderAdmAddress,
-      'sender'
-    )
-    const recipientFormatted = useCryptoAddressPretty(
-      transaction,
-      cryptoAddress,
-      recipientAdmAddress,
-      'recipient'
-    )
-
-    const explorerLink = computed(() => getExplorerTxUrl(Cryptos.ETH, props.id))
-    const blockHeight = useBlockHeight('ETH', {
-      enabled: () => transactionStatus.value === 'CONFIRMED'
-    })
-    const confirmations = computed(() => {
-      if (!blockHeight.value || !transaction.value) return NaN
-
-      if ('blockNumber' in transaction.value && transaction.value.blockNumber) {
-        return blockHeight.value - transaction.value.blockNumber + 1
-      }
-
-      return transaction.value?.confirmations
-    })
-
-    const fee = computed(() => transaction.value?.fee)
-
-    return {
-      refetch,
-      transaction,
-      fee,
-      senderFormatted,
-      recipientFormatted,
-      partnerAdmAddress,
-      explorerLink,
-      confirmations,
-      admTx,
-      queryStatus,
-      transactionStatus,
-      inconsistentStatus,
-      AllCryptos
-    }
+  id: {
+    required: true,
+    type: String
   }
 })
+
+const store = useStore()
+
+const cryptoAddress = computed(() => store.state.eth.address)
+
+const {
+  status: queryStatus,
+  isFetching,
+  data: transaction,
+  refetch
+} = useErc20TransactionQuery(props.crypto)(props.id)
+const inconsistentStatus = useInconsistentStatus(transaction, props.crypto)
+const transactionStatus = useTransactionStatus(isFetching, queryStatus, inconsistentStatus)
+useClearPendingTransaction(props.crypto, transaction)
+
+const admTx = useFindAdmTransaction(props.id)
+const senderAdmAddress = computed(() => admTx.value?.senderId || '')
+const recipientAdmAddress = computed(() => admTx.value?.recipientId || '')
+const partnerAdmAddress = computed(() =>
+  admTx.value
+    ? getPartnerAddress(admTx.value?.senderId, admTx.value?.recipientId, cryptoAddress.value)
+    : ''
+)
+
+const senderFormatted = useCryptoAddressPretty(
+  transaction,
+  cryptoAddress,
+  senderAdmAddress,
+  'sender'
+)
+const recipientFormatted = useCryptoAddressPretty(
+  transaction,
+  cryptoAddress,
+  recipientAdmAddress,
+  'recipient'
+)
+
+const explorerLink = computed(() => getExplorerTxUrl(Cryptos.ETH, props.id))
+const blockHeight = useBlockHeight('ETH', {
+  enabled: () => transactionStatus.value === 'CONFIRMED'
+})
+const confirmations = computed(() => {
+  if (!blockHeight.value || !transaction.value) return NaN
+
+  if ('blockNumber' in transaction.value && transaction.value.blockNumber) {
+    return blockHeight.value - transaction.value.blockNumber + 1
+  }
+
+  return transaction.value?.confirmations
+})
+
+const fee = computed(() => transaction.value?.fee)
 </script>
