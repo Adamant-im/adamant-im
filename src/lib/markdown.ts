6218c0a9e8fd452ebfd4a82c7b43c9e906e8e771
skranee <125743202+skranee@users.noreply.github.com>
2025-02-26 18:13:13 +0300

    chore: convert marked config to typescript and use instance

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -1,28 +1,42 @@
-import { marked } from 'marked'
+import { Marked } from 'marked'
 import DOMPurify from 'dompurify'
+
+interface IImageParams {
+  _href: string
+  _title: string
+  _text: string
+}
+
+interface ILinkParams {
+  href: string
+  _title: string
+  text: string
+}
 
 // The U+2028 character (LINE SEPARATOR) is sometimes used as a line break, but it is treated as a space in some web environments,
 // causing unexpected rendering issues. To avoid this, it's recommended to replace it with a standard line break character such as `\n`.
-const LINE_SEPARATOR = /\u2028/g
+const LINE_SEPARATOR: RegExp = /\u2028/g
 
 // LINE_BREAK_VISUAL is used for formatting when the message is displayed as a single line in a preview.
 // It replaces line breaks with a visual symbol (↵) to indicate where new lines exist in the original text.
-const LINE_BREAK_VISUAL = '↵ '
+const LINE_BREAK_VISUAL: string = '↵ '
 
-marked.setOptions({
+const markedInstance = new Marked()
+
+markedInstance.setOptions({
   // marked sanitize is deprecated, using DOMPurify
   // sanitize: true,
   gfm: true,
   breaks: true
 })
 
-const renderer = new marked.Renderer()
+const renderer = new markedInstance.Renderer()
 
-renderer.image = function ({ _href, _title, _text }) {
+renderer.image = function ({ _href, _title, _text }: IImageParams): string {
   return ''
 }
 
-renderer.link = function ({ href, _title, text }) {
+renderer.link = function ({ href, _title, text }: ILinkParams): string {
   const linkPattern = /^(eth|bch|bitcoin|https?|s?ftp|magnet|tor|onion|tg):(.*)$/i
   const emailPattern = /^(mailto):[^@]+@[^@]+\.[^@]+$/i
 
@@ -35,18 +49,18 @@ renderer.link = function ({ href, _title, text }) {
   return text
 }
 
-renderer.heading = function ({ text }) {
+renderer.heading = function ({ text }): string {
   return `<p>${text}</p>`
 }
 
-marked.use({ renderer })
+markedInstance.use({ renderer })
 
 /**
  * Sanitizes text to show HTML
  * @param {string} text text to sanitize
  * @returns {string} sanitized HTML
  */
-export function sanitizeHTML(text = '') {
+export function sanitizeHTML(text: string = ''): string {
   return DOMPurify.sanitize(text)
 }
 
@@ -55,8 +69,8 @@ export function sanitizeHTML(text = '') {
  * @param {string} text text to render
  * @returns {string} resulting sanitized HTML
  */
-export function renderMarkdown(text = '') {
-  return marked.parse(sanitizeHTML(text.replace(LINE_SEPARATOR, '\n')))
+export function renderMarkdown(text: string = ''): string {
+  return markedInstance.parse(sanitizeHTML(text.replace(LINE_SEPARATOR, '\n')))
 }
 
 /**
@@ -65,19 +79,19 @@ export function renderMarkdown(text = '') {
  * @param {string} text text to process
  * @returns {string} resulting clear text of the first line
  */
-export function removeFormats(text = '') {
+export function removeFormats(text: string = ''): string | null {
   const node = document.createElement('div')
   const textWithSymbol = text.replace(/\n/g, '↵ ')
-  node.innerHTML = marked.parse(sanitizeHTML(textWithSymbol))
+  node.innerHTML = markedInstance.parse(sanitizeHTML(textWithSymbol))
 
   return node.textContent || node.innerText || ''
 }
 
-export function formatMessage(text = '') {
+export function formatMessage(text: string = ''): string {
   const node = document.createElement('div')
 
   const textWithSymbol = text.replace(/\n/g, LINE_BREAK_VISUAL)
-  node.innerHTML = marked.parse(sanitizeHTML(textWithSymbol))
+  node.innerHTML = markedInstance.parse(sanitizeHTML(textWithSymbol))
 
   const textWithoutHtml = node.textContent || node.innerText || ''
 
