25c35b87355b7ffb6db0afd3f43d67ec67313ece
skranee <125743202+skranee@users.noreply.github.com>
2025-03-03 12:57:29 +0300

    hotfix: typescript error

--- src/filters/flattenData.ts
+++ src/filters/flattenData.ts
@@ -2,8 +2,8 @@
  * Flatten a nested object into a flat structure with keys representing the path to each value.
  * @param {object} obj - The nested object to flatten.
  * @param {string} [parentKey=""] - The base key for the current nesting level (used recursively).
- * @param {object} [result={}] - The object to store the flattened result (used recursively).
- * @returns {object} - A flat object where keys represent the path to the values in the original object.
+ * @param {Record<string, any>} [result={}] - The object to store the flattened result (used recursively).
+ * @returns {Record<string, any>} - A flat object where keys represent the path to the values in the original object.
  *
  * @example
  * const data = {
@@ -32,11 +32,11 @@
  * //   "children[second][age]": 15,
  * // }
  */
-const flattenData = (obj: object, parentKey: string = "", result: Record<string, any> = {}): Record<string, any> => {
+const flattenData = (obj: Record<string, any>, parentKey: string = "", result: Record<string, any> = {}): Record<string, any> => {
   for (const key in obj) {
     if (Object.prototype.hasOwnProperty.call(obj, key)) {
       const newKey = parentKey ? `${parentKey}[${key}]` : key;
-      if (typeof obj[key] === "object" && obj[key] !== null) {
+      if (typeof obj[key] === "object" && obj[key] !== null && !Array.isArray(obj[key])) {
         flattenData(obj[key], newKey, result);
       } else {
         result[newKey] = obj[key];
