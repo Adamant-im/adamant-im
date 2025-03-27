edd7cb84bdaaf443620d94cdf3fc4acaa20e1f16
skranee <125743202+skranee@users.noreply.github.com>
2025-03-27 15:07:27 +0300

    refactor: type instead of using as

--- src/hooks/useSaveCursor.ts
+++ src/hooks/useSaveCursor.ts
@@ -1,11 +1,11 @@
 import { watch, type Ref, type ShallowRef } from 'vue'
 
 export function useSaveCursor(
-  inputRef: Readonly<ShallowRef<unknown | null>>,
+  inputRef: Readonly<ShallowRef<HTMLInputElement | null>>,
   trigger: Ref<boolean>
 ) {
   watch(trigger, () => {
-    const inputElement = inputRef.value as HTMLInputElement
+    const inputElement = inputRef.value
     if (inputElement) {
       const cursorPosition = inputElement.selectionStart || 0
 
