20e83332f486c1ee018dcf8f6a5fc994a183716c
skranee <125743202+skranee@users.noreply.github.com>
2025-04-10 14:26:14 +0300

    refactor: Votes to composition

--- src/views/Votes.vue
+++ src/views/Votes.vue
@@ -19,8 +19,8 @@
             />
             <div :class="`${className}__info`" v-html="t('votes.stake_info')" />
             <delegates-table
-              :page="pagination.page"
-              :per-page="pagination.rowsPerPage"
+              :page="paginationOptions.page"
+              :per-page="paginationOptions.rowsPerPage"
               :search-query="search"
               :waiting-for-confirmation="waitingForConfirmation"
             />
@@ -30,7 +30,7 @@
               justify="space-between"
               no-gutters
             >
-              <pagination v-if="showPagination" v-model="pagination.page" :pages="pages" />
+              <pagination v-if="showPagination" v-model="paginationOptions.page" :pages="pages" />
 
               <v-btn
                 :disabled="reviewButtonDisabled"
@@ -83,161 +83,135 @@
   </div>
 </template>
 
-<script>
+<script lang="ts" setup>
 import AppToolbarCentered from '@/components/AppToolbarCentered.vue'
 import Pagination from '@/components/Pagination.vue'
 import DelegatesTable from '@/components/DelegatesTable/DelegatesTable.vue'
-import { computed, onMounted, ref, reactive, defineComponent, watch } from 'vue'
+import { computed, onMounted, ref, reactive, watch } from 'vue'
 import { useStore } from 'vuex'
 import { useI18n } from 'vue-i18n'
 import { mdiMagnify } from '@mdi/js'
+import { DelegateDto } from '@/lib/schema/client'
+
+// It seems to be used only here so it doesn't need any special constant yet
+type Delegate = DelegateDto & {
+  _voted: boolean
+  voted: boolean
+  upvoted: boolean
+  downvoted: boolean
+  showDetails: boolean
+  forged: number
+  status: number
+}
 
 const VOTE_REQUEST_LIMIT = 30
+const className = 'delegates-view'
 
-export default defineComponent({
-  components: {
-    DelegatesTable,
-    AppToolbarCentered,
-    Pagination
-  },
-  setup() {
-    const store = useStore()
-    const { t } = useI18n()
-    const search = ref('')
-    const pagination = reactive({
-      rowsPerPage: 50,
-      sortBy: 'rank',
-      page: 1
-    })
-    const expanded = ref([])
-    const waitingForConfirmation = ref(false)
-    const dialog = ref(false)
-    const className = 'delegates-view'
+const store = useStore()
+const { t } = useI18n()
 
-    const delegates = computed(() => {
-      const delegates = store.state.delegates.delegates || {}
+const search = ref('')
+const paginationOptions = reactive({
+  rowsPerPage: 50,
+  sortBy: 'rank',
+  page: 1
+})
+const waitingForConfirmation = ref(false)
+const dialog = ref(false)
 
-      return Object.values(delegates)
-    })
-    const numOfUpvotes = computed(() => {
-      return delegates.value.filter((delegate) => delegate.upvoted && !delegate.voted).length
-    })
-    const numOfDownvotes = computed(() => {
-      return delegates.value.filter((delegate) => delegate.downvoted && delegate.voted).length
-    })
-    const totalVotes = computed(() => {
-      return delegates.value.filter((delegate) => delegate._voted).length
-    })
-    const pages = computed(() => {
-      if (delegates.value.length <= 0) {
-        return 0
-      }
+const delegates = computed<Delegate[]>(() => {
+  const delegates = store.state.delegates.delegates || {}
 
-      return Math.ceil(delegates.value.length / pagination.rowsPerPage)
-    })
-    watch(search, (newValue) => {
-      if (newValue.length > 0) {
-        pagination.page = 1
-      }
-    })
+  return Object.values(delegates)
+})
 
-    const showPagination = computed(() => search.value.length === 0)
-    const reviewButtonDisabled = computed(() => {
-      return numOfUpvotes.value + numOfDownvotes.value === 0
-    })
+const numOfUpvotes = computed(() => {
+  return delegates.value.filter((delegate) => delegate.upvoted && !delegate.voted).length
+})
+const numOfDownvotes = computed(() => {
+  return delegates.value.filter((delegate) => delegate.downvoted && delegate.voted).length
+})
+const totalVotes = computed(() => {
+  return delegates.value.filter((delegate) => delegate._voted).length
+})
+const pages = computed(() => {
+  if (delegates.value.length <= 0) {
+    return 0
+  }
 
-    onMounted(() => {
-      store.dispatch('delegates/getDelegates', {
-        address: store.state.address
-      })
-    })
+  return Math.ceil(delegates.value.length / paginationOptions.rowsPerPage)
+})
+const showPagination = computed(() => search.value.length === 0)
+const reviewButtonDisabled = computed(() => {
+  return numOfUpvotes.value + numOfDownvotes.value === 0
+})
 
-    const upVote = (id) => {
-      store.commit('delegates/upVote', id)
-    }
-    const downVote = (id) => {
-      store.commit('delegates/downVote', id)
-    }
-    const validateVotes = () => {
-      if (numOfUpvotes.value + numOfDownvotes.value > VOTE_REQUEST_LIMIT) {
-        store.dispatch('snackbar/show', {
-          message: t('votes.vote_request_limit', { limit: VOTE_REQUEST_LIMIT })
-        })
-
-        return false
-      }
-      if (store.state.balance < 50) {
-        store.dispatch('snackbar/show', {
-          message: t('votes.no_money')
-        })
-
-        return false
-      }
-
-      return true
-    }
-    const sendVotes = () => {
-      if (!validateVotes()) {
-        return
-      }
-
-      const upVotes = delegates.value
-        .filter((delegate) => delegate.upvoted && !delegate.voted)
-        .map((delegate) => `+${delegate.publicKey}`)
-      const downVotes = delegates.value
-        .filter((delegate) => delegate.downvoted && delegate.voted)
-        .map((delegate) => `-${delegate.publicKey}`)
-      const allVotes = [...upVotes, ...downVotes]
-
-      if (allVotes.length <= 0) {
-        store.dispatch('snackbar/show', {
-          message: t('votes.select_delegates')
-        })
-
-        return
-      }
-
-      waitingForConfirmation.value = true
-      dialog.value = false
-
-      store.dispatch('delegates/voteForDelegates', {
-        votes: [...upVotes, ...downVotes],
-        address: store.state.address
-      })
-
-      store.dispatch('snackbar/show', {
-        message: t('votes.sent')
-      })
-    }
-    const showConfirmationDialog = () => {
-      dialog.value = true
-    }
-
-    return {
-      search,
-      pagination,
-      expanded,
-      waitingForConfirmation,
-      dialog,
-      t,
-      className,
-      delegates,
-      numOfUpvotes,
-      numOfDownvotes,
-      totalVotes,
-      pages,
-      showPagination,
-      reviewButtonDisabled,
-      mdiMagnify,
-      upVote,
-      downVote,
-      sendVotes,
-      validateVotes,
-      showConfirmationDialog,
-      VOTE_REQUEST_LIMIT
-    }
+watch(search, (newValue) => {
+  if (newValue.length > 0) {
+    paginationOptions.page = 1
   }
 })
+
+onMounted(() => {
+  store.dispatch('delegates/getDelegates', {
+    address: store.state.address
+  })
+})
+
+const validateVotes = () => {
+  if (numOfUpvotes.value + numOfDownvotes.value > VOTE_REQUEST_LIMIT) {
+    store.dispatch('snackbar/show', {
+      message: t('votes.vote_request_limit', { limit: VOTE_REQUEST_LIMIT })
+    })
+
+    return false
+  }
+  if (store.state.balance < 50) {
+    store.dispatch('snackbar/show', {
+      message: t('votes.no_money')
+    })
+
+    return false
+  }
+
+  return true
+}
+const sendVotes = () => {
+  if (!validateVotes()) {
+    return
+  }
+
+  const upVotes = delegates.value
+    .filter((delegate) => delegate.upvoted && !delegate.voted)
+    .map((delegate) => `+${delegate.publicKey}`)
+  const downVotes = delegates.value
+    .filter((delegate) => delegate.downvoted && delegate.voted)
+    .map((delegate) => `-${delegate.publicKey}`)
+  const allVotes = [...upVotes, ...downVotes]
+
+  if (allVotes.length <= 0) {
+    store.dispatch('snackbar/show', {
+      message: t('votes.select_delegates')
+    })
+
+    return
+  }
+
+  waitingForConfirmation.value = true
+  dialog.value = false
+
+  store.dispatch('delegates/voteForDelegates', {
+    votes: [...upVotes, ...downVotes],
+    address: store.state.address
+  })
+
+  store.dispatch('snackbar/show', {
+    message: t('votes.sent')
+  })
+}
+const showConfirmationDialog = () => {
+  dialog.value = true
+}
 </script>
 
 <style lang="scss" scoped>
