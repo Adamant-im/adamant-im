84762d1b28ac1bb198f7114d5f7e560f6dedbb00
Serg <s.frontend.dev@gmail.com>
2025-04-15 21:39:32 +0700

    chore: remove unused vars

--- src/views/Nodes.vue
+++ src/views/Nodes.vue
@@ -8,16 +8,7 @@ import { nodesManager } from '@/lib/nodes'
 
 export default {
   components: {
-    NodesTable,
-  },
-  data: () => ({
-    pagination: {
-      sortBy: 'name'
-    },
-    timer: null
-  }),
-  computed: {
-    className: () => 'nodes-view'
+    NodesTable
   },
   mounted() {
     this.$store.dispatch('nodes/updateStatus')
