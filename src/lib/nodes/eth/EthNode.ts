3828ade5ffce4ece297da6370929ef9475fe4738
Defined Prototype <5364018+graycraft@users.noreply.github.com>
2025-04-22 02:02:23 +0500

    feat(lib/nodes/eth/EthNode.ts): request a node by IP if domain is not available

--- src/lib/nodes/eth/EthNode.ts
+++ src/lib/nodes/eth/EthNode.ts
@@ -16,11 +16,15 @@ export class EthNode extends Node<Web3Eth> {
   }
 
   protected buildClient(): Web3Eth {
-    return new Web3Eth(new HttpProvider(this.url))
+    return new Web3Eth(new HttpProvider(this.preferAltIp ? (this.alt_ip as string) : this.url))
   }
 
   protected async checkHealth() {
     const time = Date.now()
+
+    /** Set `clientUrl` for `HttpProvider` depending on `preferAltIp`. */
+    this.client = this.buildClient()
+
     const blockNumber = await this.client.getBlockNumber()
 
     return {
