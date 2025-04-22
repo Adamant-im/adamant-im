123818fb417c4065adc107fe57f2de216a7b553a
Defined Prototype <5364018+graycraft@users.noreply.github.com>
2025-04-22 21:27:52 +0500

    refactor(config/index.js): use vite mode instead of custom env var; remove test mode

--- src/config/index.js
+++ src/config/index.js
@@ -1,20 +1,15 @@
 import devConfig from './development.json'
 import prodConfig from './production.json'
-import testingConfig from './test.json'
 import testnetConfig from './testnet.json'
 import torConfig from './tor.json'
 
 const configMap = {
   development: devConfig,
   production: prodConfig,
-  test: testingConfig,
   testnet: testnetConfig,
   tor: torConfig
 }
 
-let configName = import.meta.env.VITE_ADM_CONFIG_FILE
-
-if (!(configName in configMap))
-  configName = import.meta.env.MODE
+const configName = import.meta.env.MODE
 
 export default configMap[configName]
