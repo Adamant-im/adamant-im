fed6058b6be3ef6fc9495df64416efb5182513b3
Defined Prototype <5364018+graycraft@users.noreply.github.com>
2025-04-27 15:06:25 +0500

    ci(.github/workflows/electron-*.yml): set `VITE_ROUTER_HISTORY_MODE` env var on build step

--- vite-pwa.config.ts
+++ vite-pwa.config.ts
@@ -1,16 +1,12 @@
-import { defineConfig, mergeConfig } from 'vite'
+import { mergeConfig, defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 import viteBaseConfig from './vite-base.config'
 import { manifest } from './vite-config/manifest'
 
-export default defineConfig(() => {
-  /** @see https://vite.dev/guide/api-javascript.html#mergeconfig */
-  return mergeConfig(viteBaseConfig, {
-    /** @see https://vite.dev/config/shared-options.html#define */
-    define: {
-      __VITE_ROUTER_HISTORY_MODE__: JSON.stringify(process.env.VITE_ROUTER_HISTORY_MODE)
-    },
+export default mergeConfig(
+  viteBaseConfig,
+  defineConfig({
     plugins: [
       VitePWA({
         registerType: 'autoUpdate',
@@ -28,4 +24,4 @@ export default defineConfig(() => {
       })
     ]
   })
-})
+)
