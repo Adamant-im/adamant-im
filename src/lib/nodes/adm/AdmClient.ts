9fa1f69486cdaf1e7f21e55de7a6775a4c2e699a
skranee <125743202+skranee@users.noreply.github.com>
2025-05-25 19:39:18 +0300

    feat: always use default delta and rename the constant

--- src/lib/nodes/adm/AdmClient.ts
+++ src/lib/nodes/adm/AdmClient.ts
@@ -4,7 +4,7 @@ import {
   GetHeightResponseDto,
   RegisterChatMessageTransaction
 } from '@/lib/schema/client'
-import { defaultTimeDelta, NODE_LABELS } from '@/lib/nodes/constants'
+import { DEFAULT_TIME_DELTA, NODE_LABELS } from '@/lib/nodes/constants'
 import { AdmNode, Payload, RequestConfig } from './AdmNode'
 import { Client } from '../abstract.client'
 
@@ -72,12 +72,7 @@ export class AdmClient extends Client<AdmNode> {
   }
 
   getTimeDelta() {
-    const areNodesOnline = this.nodes.some((node) => node.getNodeStatus() === 'online')
-    if (!areNodesOnline) {
-      return defaultTimeDelta
-    }
-
-    return this.getNode().timeDelta
+    return DEFAULT_TIME_DELTA
   }
 
   async sendChatTransaction(
