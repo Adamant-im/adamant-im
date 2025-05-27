30d703d264d505d7528125890e2c6d9e972aa415
skranee <125743202+skranee@users.noreply.github.com>
2025-05-27 12:10:02 +0300

    update useBalanceCheck hook

--- src/hooks/useBalanceCheck.ts
+++ src/hooks/useBalanceCheck.ts
@@ -1,13 +1,52 @@
 import { useStore } from 'vuex'
 import { useNow } from '@vueuse/core'
-import { computed } from 'vue'
+import { computed, onMounted, onUnmounted, ref, watch } from 'vue'
+import Visibility from 'visibilityjs'
+import { NodeStatusResult } from '@/lib/nodes/abstract.node'
 
 export function useBalanceCheck() {
   const store = useStore()
   const { now } = useNow({ interval: 500, controls: true })
 
+  const visibilityId = ref<number | boolean | null>(null)
+
+  const isOnline = computed(() => store.getters['isOnline'])
+  const coinNodes = computed<NodeStatusResult[]>(() => store.getters['nodes/coins'])
+  const areCoinNodesOnline = computed(() =>
+    coinNodes.value.some((node) => node.status === 'online')
+  )
   const balanceActualUntil = computed(() => store.state.balanceActualUntil)
   const currentTime = computed(() => now.value.getTime())
+
+  const updateBalance = async () => {
+    await store.dispatch('updateBalance', {
+      requestedByUser: true
+    })
+  }
+
+  watch(isOnline, async (newValue) => {
+    if (newValue) {
+      await updateBalance()
+    }
+  })
+
+  watch(areCoinNodesOnline, async (newValue) => {
+    if (newValue) {
+      await updateBalance()
+    }
+  })
+
+  onMounted(() => {
+    visibilityId.value = Visibility.change(async (event, state) => {
+      if (state === 'visible') {
+        await updateBalance()
+      }
+    })
+  })
+
+  onUnmounted(() => {
+    Visibility.unbind(Number(visibilityId.value))
+  })
 
   return computed(() => balanceActualUntil.value > currentTime.value)
 }
