b57470845616e962249bcfd389eeee5a691b4f9f
Defined Prototype <5364018+graycraft@users.noreply.github.com>
2025-06-21 00:53:55 +0500

    refactor(src/lib/nodes/eth-indexer/EthIndexer.ts): do not check node URL on client initialization

--- src/lib/nodes/eth-indexer/EthIndexer.ts
+++ src/lib/nodes/eth-indexer/EthIndexer.ts
@@ -15,9 +15,7 @@ export class EthIndexer extends Node<AxiosInstance> {
   }
 
   protected buildClient(): AxiosInstance {
-    const baseURL = getBaseURL(this)
-
-    return axios.create({ baseURL })
+    return axios.create({ baseURL: this.url })
   }
 
   /**
