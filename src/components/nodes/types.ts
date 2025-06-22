67fe1526d8ffab1ad3c5d3aae82ed25ef4b52eac
skranee <125743202+skranee@users.noreply.github.com>
2025-06-22 18:49:54 +0300

    refactor: type Tab declaration

--- src/components/nodes/types.ts
+++ src/components/nodes/types.ts
@@ -1,8 +1,8 @@
-export type Tab = 'adm' | 'coins' | 'services' | 'ipfs'
-
 export const TABS = {
   adm: 'adm',
   coins: 'coins',
   services: 'services',
   ipfs: 'ipfs'
 } as const
+
+export type Tab = keyof typeof TABS
