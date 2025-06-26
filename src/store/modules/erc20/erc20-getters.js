42eac30c355f822db0b1117136e8926d6864cfb9
Timur Sultanov <p3general@gmail.com>
2025-06-26 18:21:08 +0300

    fix: code-review fixes

--- src/store/modules/erc20/erc20-getters.js
+++ src/store/modules/erc20/erc20-getters.js
@@ -1,35 +1,11 @@
 import baseGetters from '../eth-base/eth-base-getters'
-import { calculateFee } from '../../../lib/eth-utils'
-import { CryptosInfo } from '../../../lib/constants'
 
 export default {
   gasPrice(state, getters, rootState, rootGetters) {
     return rootGetters['eth/gasPrice']
   },
 
-  fee:
-    (state, getters) =>
-    (_amount, _address, _textData, _isNew, increaseFee = false) => {
-      const gasPrice = getters.gasPrice
-      if (!gasPrice) return 0
-
-      const cryptoInfo = CryptosInfo[state.crypto]
-      if (!cryptoInfo) return 0
-
-      const baseGasLimit = cryptoInfo.defaultGasLimit || 58000
-      const reliabilityGasLimitPercent = cryptoInfo.reliabilityGasLimitPercent || 10
-      const finalGasLimit = baseGasLimit + (baseGasLimit * reliabilityGasLimitPercent) / 100
-
-      const reliabilityGasPricePercent = cryptoInfo.reliabilityGasPricePercent || 10
-      let finalGasPrice = gasPrice + (gasPrice * reliabilityGasPricePercent) / 100
-
-      if (increaseFee) {
-        const increasedGasPricePercent = cryptoInfo.increasedGasPricePercent || 30
-        finalGasPrice = finalGasPrice + (finalGasPrice * increasedGasPricePercent) / 100
-      }
-
-      return calculateFee(Math.round(finalGasLimit), Math.round(finalGasPrice))
-    },
+  fee: (state) => state.fee,
 
   ...baseGetters
 }
--- src/store/modules/erc20/erc20-state.js
+++ src/store/modules/erc20/erc20-state.js
@@ -5,6 +5,9 @@ export default function initialState(crypto, contractAddress, decimals) {
     crypto,
     contractAddress,
     decimals,
+    gasPrice: 0,
+    gasLimit: 0,
+    fee: 0,
     ...baseState()
   }
 }
