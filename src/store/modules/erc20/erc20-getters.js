4b138657d8cdc2be6474eecda59da242b1fb12b6
Timur Sultanov <p3general@gmail.com>
2025-07-18 17:58:19 +0300

    fix: code review fixes

--- src/store/modules/erc20/erc20-getters.js
+++ src/store/modules/erc20/erc20-getters.js
@@ -1,6 +1,7 @@
 import baseGetters from '../eth-base/eth-base-getters'
 import { CryptosInfo } from '@/lib/constants'
 import { calculateReliableValue, calculateFee } from '@/lib/eth-utils'
+import { BigNumber } from 'bignumber.js'
 
 export default {
   gasPrice(state, getters, rootState, rootGetters) {
@@ -9,26 +10,41 @@ export default {
 
   finalGasPrice: (state, getters, rootState, rootGetters) => (increaseFee) => {
     const ethGasPrice = rootGetters['eth/gasPrice']
-    const tokenInfo = CryptosInfo[state.crypto]
+    const ethInfo = CryptosInfo['ETH']
 
-    const reliableGasPrice = Number(ethGasPrice) * (1 + tokenInfo.reliabilityGasPricePercent / 100)
-
-    if (increaseFee) {
-      return reliableGasPrice * (1 + tokenInfo.increasedGasPricePercent / 100)
-    }
-
-    return reliableGasPrice
-  },
-
-  fee: (state, getters) => (amount, address, textData, isNewAccount, increaseFee) => {
-    const finalGasPrice = getters.finalGasPrice(increaseFee)
-    const reliableGasLimit = calculateReliableValue(
-      CryptosInfo[state.crypto].defaultGasLimit,
-      CryptosInfo[state.crypto].reliabilityGasLimitPercent
+    const reliableGasPrice = calculateReliableValue(
+      BigNumber(ethGasPrice).toNumber(),
+      ethInfo.reliabilityGasPricePercent
     )
 
-    return Number(calculateFee(Math.round(reliableGasLimit), Math.round(finalGasPrice)))
+    if (increaseFee) {
+      const increasedGasPrice = calculateReliableValue(
+        reliableGasPrice,
+        ethInfo.increasedGasPricePercent
+      )
+      return BigNumber(increasedGasPrice).integerValue().toString()
+    }
+
+    return BigNumber(reliableGasPrice).integerValue().toString()
   },
+
+  fee:
+    (state, getters) =>
+    (amount, address, textData, isNewAccount, increaseFee, estimatedGasLimit = null) => {
+      const finalGasPrice = getters.finalGasPrice(increaseFee)
+      const tokenInfo = CryptosInfo[state.crypto]
+      const ethInfo = CryptosInfo['ETH']
+
+      let gasLimit
+
+      if (estimatedGasLimit && amount > 0) {
+        gasLimit = calculateReliableValue(estimatedGasLimit, ethInfo.reliabilityGasLimitPercent)
+      } else {
+        gasLimit = tokenInfo.defaultGasLimit
+      }
+
+      return Number(calculateFee(Math.round(gasLimit), finalGasPrice))
+    },
 
   ...baseGetters
 }
