2488123115bbf9532abf765a1c6277a2a46703e2
Serg <s.frontend.dev@gmail.com>
2025-08-07 21:46:21 +0700

    fix: rollback local run

--- scripts/capacitor/build-android.mjs
+++ scripts/capacitor/build-android.mjs
@@ -1,7 +1,5 @@
 import dotenv from 'dotenv'
 import { $ } from 'execa'
-import { renameSync } from 'fs'
-import { readFileSync } from 'fs'
 
 dotenv.config({
   path: 'capacitor.env'
@@ -24,11 +22,4 @@ async function run() {
     '--signing-type apksigner'
   ]
   await $$`cap build android ${buildArgs}`
-
-
-  const version = JSON.parse(readFileSync('package.json', 'utf-8')).version
-  const src = 'android/app/build/outputs/apk/release/app-release-signed.apk'
-  const dest = `android/app/build/outputs/apk/release/ADAMANT.Messenger-${version}-signed.apk`
-
-  renameSync(src, dest)
 }
