4afc731c7424b096abc3ffa632e09e6444e45dc8
Defined Prototype <5364018+graycraft@users.noreply.github.com>
2025-08-28 22:12:43 +0500

    chore(src/lib/nodes/abstract.node.ts): print healthcheck messages verbosely

--- src/lib/nodes/abstract.node.ts
+++ src/lib/nodes/abstract.node.ts
@@ -153,24 +153,38 @@ export abstract class Node<C = unknown> {
 
         if (this.online) {
           if (this.preferDomain) {
-            console.info(`Connect by domain ${this.url} succeeded.`)
+            console.info(`[HealthCheck] Connect by domain ${this.url} succeeded.`)
             this.firstDomainAttempt = false
           } else {
-            console.info(`Connect by IP ${this.altIp} succeeded.`)
+            console.info(`[HealthCheck] Connect by IP ${this.altIp} succeeded.`)
           }
-        }
-      } catch {
-        if (this.preferDomain && this.firstDomainAttempt) {
-          console.info(
-            `Connect by domain ${this.url} failed. Will try to connect by IP ${this.altIp}.`
-          )
-          this.preferDomain = false
         } else {
           console.info(
-            `Connect by domain ${this.url} and by IP ${this.altIp} failed. Node is offline.`
+            `[HealthCheck] Node with domain ${this.url} and IP ${this.altIp} is offline.`
           )
-          this.online = false
-          this.preferDomain = true
+        }
+      } catch {
+        if (this.online) {
+          if (this.preferDomain && this.firstDomainAttempt) {
+            if (this.altIp) {
+              console.info(
+                `[HealthCheck] Connect by domain ${this.url} failed. Will try to connect by IP ${this.altIp}.`
+              )
+              this.preferDomain = false
+            } else {
+              console.info(`[HealthCheck] Connect by domain ${this.url} failed.`)
+            }
+          } else {
+            if (this.altIp) {
+              console.info(
+                `[HealthCheck] Connect by domain ${this.url} and by IP ${this.altIp} failed. Node is offline.`
+              )
+              this.preferDomain = true
+            } else {
+              console.info(`[HealthCheck] Connect by domain ${this.url} failed. Node is offline.`)
+            }
+            this.online = false
+          }
         }
       } finally {
         this.updateURL()
