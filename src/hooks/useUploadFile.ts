57870b44b60d2bdd8fbe2fb8ebc95ba5f67e7901
Timur Sultanov <p3general@gmail.com>
2026-01-04 22:37:22 +0800

    chore: fix type errors

--- src/hooks/useUploadFile.ts
+++ src/hooks/useUploadFile.ts
@@ -3,7 +3,7 @@ import { watch } from 'vue'
 import ipfs from '@/lib/nodes/ipfs'
 import { useMutation } from '@tanstack/vue-query'
 
-type ParsedFile = { file: File; binary: Buffer | Uint8Array }
+type ParsedFile = { file: File; binary: Uint8Array<ArrayBuffer> }
 
 export async function readFile(file: File): Promise<ArrayBuffer> {
   return new Promise((resolve, reject) => {
