f38409c6178de4a5bcd201ce73abaf53f619e925
Timur Sultanov <p3general@gmail.com>
2026-01-27 23:24:04 +0800

    chore: upgrade dependencies and fix ECPair v3 validation
    - Update bitcoinjs-lib to v7 and ecpair to v3
    - Update utility packages: uuid, bignumber.js, dompurify, marked, semver
    - Update dev dependencies: lint-staged, commitlint
    - Fix 'ValiError' in networks.js by ensuring 'bech32' is always a string,
      satisfying new ECPair strict runtime validation rules.

--- src/lib/bitcoin/networks.js
+++ src/lib/bitcoin/networks.js
@@ -1,8 +1,15 @@
 import coininfo from 'coininfo'
 import { Cryptos } from '../constants'
 
-export default Object.freeze({
+const nets = {
   [Cryptos.DOGE]: coininfo.dogecoin.main.toBitcoinJS(),
   [Cryptos.DASH]: coininfo.dash.main.toBitcoinJS(),
   [Cryptos.BTC]: coininfo.bitcoin.main.toBitcoinJS()
+}
+
+// fix lack of bech32 value as new validation rules of ecpair dependency require string value in this key
+Object.values(nets).forEach((net) => {
+  net.bech32 ??= ''
 })
+
+export default Object.freeze(nets)
