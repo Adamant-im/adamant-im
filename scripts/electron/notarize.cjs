0814f8ee9e6b8be9adc62bb85bde6df79b8bdab0
metalisk <metaliskapp@gmail.com>
2026-02-20 10:14:50 +0300

    fix: replace logger with console for notarization logging

--- scripts/electron/notarize.cjs
+++ scripts/electron/notarize.cjs
@@ -1,7 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-import { logger } from '@/utils/devTools/logger'
-
+// `electron-builder` notarize hook is loaded as CommonJS.
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const { notarize } = require('@electron/notarize')
+const logInfo = (...args) => console.info('[notarizing]', ...args)
 
 exports.default = async function notarizing(context) {
   const { electronPlatformName, appOutDir } = context
@@ -11,13 +11,13 @@ exports.default = async function notarizing(context) {
   }
 
   if (process.env.APPLE_NOTARIZE !== 'true') {
-    logger.log('notarizing', 'info', 'APPLE_NOTARIZE=false | Skipping the notarization"')
+    logInfo('APPLE_NOTARIZE=false | Skipping the notarization')
     return
   }
 
   const appName = context.packager.appInfo.productFilename
 
-  logger.log("notarizing", 'info', `Preparing the app ${appName} for notarization`)
+  logInfo(`Preparing the app ${appName} for notarization`)
 
   return notarize({
     appPath: `${appOutDir}/${appName}.app`,
